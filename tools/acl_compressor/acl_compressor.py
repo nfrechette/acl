50b742a5fb1b137e70cd603e1d69034fd21dc707
Nicholas Frechette <zeno490@gmail.com>
2017-07-08 13:38:30 -0400

    Adding -parallel=N switch to acl_compressor.py to generate stats faster.

--- tools/acl_compressor/acl_compressor.py
+++ tools/acl_compressor/acl_compressor.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import queue
+import threading
 from collections import namedtuple
 
 Stats = namedtuple('Stats', 'filename name rotation_format translation_format range_reduction raw_size compressed_size ratio max_error compression_time duration num_animated_tracks')
@@ -11,22 +13,26 @@ def parse_argv():
 	options['stats'] = ""
 	options['csv'] = False
 	options['refresh'] = False
+	options['num_threads'] = 1
 
 	for i in range(1, len(sys.argv)):
 		value = sys.argv[i]
 
 		# TODO: Strip trailing '/' or '\'
 		if value.startswith('-acl='):
-			options['acl'] = value[5:].replace('"', '')
+			options['acl'] = value[len('-acl='):].replace('"', '')
 
 		if value.startswith('-stats='):
-			options['stats'] = value[7:].replace('"', '')
+			options['stats'] = value[len('-stats='):].replace('"', '')
 
 		if value == '-csv':
 			options['csv'] = True
 
 		if value == '-refresh':
 			options['refresh'] = True
+
+		if value.startswith('-parallel='):
+			options['num_threads'] = int(value[len('-parallel='):].replace('"', ''))
 
 	if options['acl'] == None:
 		print('ACL input directory not found')
@@ -38,10 +44,15 @@ def parse_argv():
 		print_usage()
 		sys.exit(1)
 
+	if options['num_threads'] <= 0:
+		print('-parallel switch argument must be greater than 0')
+		print_usage()
+		sys.exit(1)
+
 	return options
 
 def print_usage():
-	print('Usage: python acl_compressor.py -acl=<path to directory containing ACL files> -stats=<path to output directory for stats> [-csv] [-refresh]')
+	print('Usage: python acl_compressor.py -acl=<path to directory containing ACL files> -stats=<path to output directory for stats> [-csv] [-refresh] [-parallel={Num Threads}]')
 
 def print_stat(stat):
 	print('Algorithm: {}, Format: [{}, {}, {}], Ratio: {:.2f}, Error: {}'.format(stat.name, stat.rotation_format, stat.translation_format, stat.range_reduction, stat.ratio, stat.max_error))
@@ -66,6 +77,17 @@ def output_csv(stat_dir):
 		print('{}, {}, {}, {}, {}, {}, {}, {}, {}, {}'.format(stat.name, rotation_format, translation_format, range_reduction, stat.raw_size, stat.compressed_size, stat.ratio, stat.compression_time, stat.duration, stat.num_animated_tracks), file = file)
 	file.close()
 
+def run_acl_compressor(cmd_queue):
+	while True:
+		entry = cmd_queue.get()
+		if entry is None:
+			return
+
+		(acl_filename, cmd) = entry
+
+		print('Compressing {}...'.format(acl_filename))
+		os.system(cmd)
+
 if __name__ == "__main__":
 	options = parse_argv()
 
@@ -89,6 +111,7 @@ if __name__ == "__main__":
 		sys.exit(1)
 
 	stat_files = []
+	cmd_queue = queue.Queue()
 
 	for (dirpath, dirnames, filenames) in os.walk(acl_dir):
 		stat_dirname = dirpath.replace(acl_dir, stat_dir)
@@ -110,13 +133,37 @@ if __name__ == "__main__":
 
 			cmd = '{} -acl="{}" -stats="{}"'.format(compressor_exe_path, acl_filename, stat_filename)
 			cmd = cmd.replace('/', '\\')
-
-			print('Compressing {}...'.format(acl_filename))
-			os.system(cmd)
+			cmd_queue.put((acl_filename, cmd))
 
 	if len(stat_files) == 0:
 		sys.exit(0)
 
+	if not cmd_queue.empty():
+		# Add a marker to terminate the threads
+		for i in range(options['num_threads']):
+			cmd_queue.put(None)
+
+		threads = [ threading.Thread(target = run_acl_compressor, args = (cmd_queue,)) for _i in range(options['num_threads']) ]
+		for thread in threads:
+			thread.daemon = True
+			thread.start()
+
+		try:
+			while True:
+				for thread in threads:
+					thread.join(1.0)
+
+				all_threads_done = True
+				for thread in threads:
+					if thread.isAlive():
+						all_threads_done = False
+
+				if all_threads_done:
+					break
+		except KeyboardInterrupt:
+			sys.exit(1)
+
+	print('')
 	print('Aggregating results...')
 	print('')
 
