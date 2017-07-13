79d96ff37fc0ad098205650e06272cf113a5a0be
Nicholas Frechette <zeno490@gmail.com>
2017-07-12 21:21:53 -0400

    Adding core memory unit tests.

--- tests/sources/test_core_memory.cpp
+++ tests/sources/test_core_memory.cpp
@@ -6,7 +6,7 @@ using namespace acl;
 
 TEST_CASE("memcpy_bits", "[core][memory]")
 {
-	uint64_t dest = byte_swap(~0ull);
+	uint64_t dest = ~0ull;
 	uint64_t src = byte_swap(0x5555555555555555ull);
 	memcpy_bits(&dest, 1, &src, 0, 64 - 3);
 	REQUIRE(dest == byte_swap(0xAAAAAAAAAAAAAAABull));
@@ -20,4 +20,15 @@ TEST_CASE("memcpy_bits", "[core][memory]")
 	src = byte_swap(0x3800000000000000ull);
 	memcpy_bits(&dest, 1, &src, 2, 5);
 	REQUIRE(dest == byte_swap(0x7300FF0000000000ull));
+
+	dest = 0;
+	src = ~0ull;
+	memcpy_bits(&dest, 1, &src, 0, 7);
+	REQUIRE(dest == byte_swap(0x7F00000000000000ull));
+
+	memcpy_bits(&dest, 8, &src, 0, 8);
+	REQUIRE(dest == byte_swap(0x7FFF000000000000ull));
+
+	memcpy_bits(&dest, 0, &src, 0, 64);
+	REQUIRE(dest == ~0ull);
 }
