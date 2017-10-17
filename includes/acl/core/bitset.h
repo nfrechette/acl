c6879384965e632f21db8a460a16e22a2d54d820
Nicholas Frechette <zeno490@gmail.com>
2017-10-16 23:46:29 -0400

    Fix undefined behavior in bitset_test.

--- includes/acl/core/bitset.h
+++ includes/acl/core/bitset.h
@@ -16,7 +16,7 @@ namespace acl
 
 	inline void bitset_reset(uint32_t* bitset, uint32_t size, bool value)
 	{
-		uint32_t mask = value ? 0xFFFFFFFF : 0x00000000;
+		const uint32_t mask = value ? 0xFFFFFFFF : 0x00000000;
 
 		for (uint32_t offset = 0; offset < size; ++offset)
 			bitset[offset] = mask;
@@ -24,10 +24,10 @@ namespace acl
 
 	inline void bitset_set(uint32_t* bitset, uint32_t size, uint32_t bit_offset, bool value)
 	{
-		ACL_ENSURE(bit_offset < get_bitset_num_bits(size), "Invalid bit offset: %u > %u", bit_offset, get_bitset_num_bits(size));
+		ACL_ENSURE(bit_offset < get_bitset_num_bits(size), "Invalid bit offset: %u >= %u", bit_offset, get_bitset_num_bits(size));
 
-		uint32_t offset = bit_offset / 32;
-		uint32_t mask = 1 << (31 - (bit_offset - offset));
+		const uint32_t offset = bit_offset / 32;
+		const uint32_t mask = 1 << (31 - (bit_offset % 32));
 
 		if (value)
 			bitset[offset] |= mask;
@@ -37,10 +37,10 @@ namespace acl
 
 	inline bool bitset_test(const uint32_t* bitset, uint32_t size, uint32_t bit_offset)
 	{
-		ACL_ENSURE(bit_offset < get_bitset_num_bits(size), "Invalid bit offset: %u > %u", bit_offset, get_bitset_num_bits(size));
+		ACL_ENSURE(bit_offset < get_bitset_num_bits(size), "Invalid bit offset: %u >= %u", bit_offset, get_bitset_num_bits(size));
 
-		uint32_t offset = bit_offset / 32;
-		uint32_t mask = 1 << (31 - (bit_offset - offset));
+		const uint32_t offset = bit_offset / 32;
+		const uint32_t mask = 1 << (31 - (bit_offset % 32));
 
 		return (bitset[offset] & mask) != 0;
 	}
