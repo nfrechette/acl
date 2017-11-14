f948e3dc1523a25056db9fd597d91d8b307fc474
Nicholas Frechette <zeno490@gmail.com>
2017-11-13 22:20:02 -0500

    Refactored bit rate graph generation script to use an SJSON input file. Added an SJSON input file for CMU.

--- tools/graph_generation/gen_bit_rate_graph.py
+++ tools/graph_generation/gen_bit_rate_graph.py
@@ -1,36 +1,58 @@
 import numpy
+import os
+import sys
 
-input_data_type_def = {
-	'names': ('algorithm_names', '0', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '32'),
-	'formats': ('S128', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4')
-}
-columns_to_extract = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
+# This script depends on a SJSON parsing package:
+# https://pypi.python.org/pypi/SJSON/1.1.0
+# https://shelter13.net/projects/SJSON/
+# https://bitbucket.org/Anteru/sjson/src
+import sjson
 
-output_csv_file_path = 'D:\\acl-dev\\tools\\graph_generation\\bit_rates.csv'
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print('Usage: python gen_bit_rate_graph.py <path/to/input_file.sjson>')
+		sys.exit(1)
 
-input_csv_files =  []
-input_csv_files.append(('ACL v0.5 @ 0.01cm', columns_to_extract, 'D:\\test_animations\\carnegie-mellon-acl-stats-0.5.0-detailed\\stats_bit_rate.csv'))
+	input_sjson_file = sys.argv[1]
+	if not input_sjson_file.endswith('.sjson'):
+		print('Expected SJSON input file, found: {}'.format(input_sjson_file))
+		sys.exit(1)
 
-acl_best_algorithm_name = b'R:QuatNoWVar|T:Vec3Var|S:Vec3Var Clip RR:Rot|Trans|Scale Segment RR:Rot|Trans|Scale'
+	if not os.path.exists(input_sjson_file):
+		print('Input file not found: {}'.format(input_sjson_file))
+		sys.exit(1)
 
-output_csv_data = []
-output_csv_headers = ['Bit Rate']
+	with open(input_sjson_file, 'r') as file:
+		input_sjson_data = sjson.loads(file.read())
 
-output_csv_data.append(['0', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '32'])
+	input_data_type_def = {
+		'names': ('algorithm_names', '0', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '32'),
+		'formats': ('S128', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4', 'f4')
+	}
+	columns_to_extract = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19)
 
-for (header, columns_to_extract, input_csv_file_path) in input_csv_files:
-	csv_data = numpy.loadtxt(input_csv_file_path, delimiter=',', dtype=input_data_type_def, skiprows=1, usecols=columns_to_extract)
-	best_variable_data_mask = csv_data['algorithm_names'] == acl_best_algorithm_name
-	csv_data = csv_data[best_variable_data_mask]
+	output_csv_file_path = 'D:\\acl-dev\\tools\\graph_generation\\bit_rates.csv'
 
-	# Strip algorithm name
-	output_csv_data.append(csv_data[0].tolist()[1:])
+	output_csv_data = []
+	output_csv_headers = ['Bit Rate']
 
-	output_csv_headers.append(header)
+	output_csv_data.append(['0', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '32'])
 
-output_csv_data = numpy.column_stack(output_csv_data)
+	for entry in input_sjson_data['inputs']:
+		csv_data = numpy.loadtxt(entry['file'], delimiter=',', dtype=input_data_type_def, skiprows=1, usecols=columns_to_extract)
 
-with open(output_csv_file_path, 'wb') as f:
-	header = bytes('{}\n'.format(','.join(output_csv_headers)), 'utf-8')
-	f.write(header)
-	numpy.savetxt(f, output_csv_data, delimiter=',', fmt=('%s'))
+		if 'filter' in entry and entry['filter'] != None:
+			best_variable_data_mask = csv_data['algorithm_names'] == bytes(entry['filter'], encoding = 'utf-8')
+			csv_data = csv_data[best_variable_data_mask]
+
+		# Strip algorithm name
+		output_csv_data.append(csv_data[0].tolist()[1:])
+
+		output_csv_headers.append(entry['header'])
+
+	output_csv_data = numpy.column_stack(output_csv_data)
+
+	with open(output_csv_file_path, 'wb') as f:
+		header = bytes('{}\n'.format(','.join(output_csv_headers)), 'utf-8')
+		f.write(header)
+		numpy.savetxt(f, output_csv_data, delimiter=',', fmt=('%s'))
