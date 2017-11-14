02c5db237c0615a92337fe4a2de702ff26aa3d1b
Nicholas Frechette <zeno490@gmail.com>
2017-11-13 21:14:28 -0500

    Adding v0.5 stats and a csv for clip durations.

--- tools/graph_generation/gen_summary_distribution.py
+++ tools/graph_generation/gen_summary_distribution.py
@@ -1,14 +1,15 @@
 import numpy
 
-input_data_type_def = { 'names': ('algorithm_names', 'raw_sizes', 'compression_ratios', 'max_errors'), 'formats': ('S128', 'f4', 'f4', 'f4') }
-columns_to_extract_acl = (0, 1, 3, 7)
-columns_to_extract_ue4 = (0, 1, 3, 5)
+input_data_type_def = { 'names': ('algorithm_names', 'raw_sizes', 'compression_ratios', 'durations', 'max_errors'), 'formats': ('S128', 'f4', 'f4', 'f4', 'f4') }
+columns_to_extract_acl = (0, 1, 3, 5, 7)
+columns_to_extract_ue4 = (0, 1, 3, 4, 5)
 
 output_csv_file_path_ratios = 'D:\\acl-dev\\tools\\graph_generation\\compression_ratios.csv'
 output_csv_file_path_ratios_by_raw_size = 'D:\\acl-dev\\tools\\graph_generation\\compression_ratios_by_raw_size.csv'
 output_csv_file_path_max_errors = 'D:\\acl-dev\\tools\\graph_generation\\max_errors.csv'
 output_csv_file_path_max_errors_by_raw_size = 'D:\\acl-dev\\tools\\graph_generation\\max_errors_by_raw_size.csv'
 output_csv_file_path_ratio_vs_max_error = 'D:\\acl-dev\\tools\\graph_generation\\ratio_vs_max_error.csv'
+output_csv_file_path_durations = 'D:\\acl-dev\\tools\\graph_generation\\durations.csv'
 
 input_csv_files =  []
 input_csv_files.append(('ACL v0.4 @ 0.01cm', columns_to_extract_acl, 'D:\\test_animations\\carnegie-mellon-acl-stats-0.4.0-summary-fixed-ratio\\stats_summary.csv'))
@@ -25,6 +26,7 @@ output_csv_data_ratios_by_raw_size = []
 output_csv_data_max_errors = []
 output_csv_data_max_errors_by_raw_size = []
 output_csv_data_ratio_vs_max_error = []
+output_csv_data_durations = []
 output_csv_headers = []
 
 for (header, columns_to_extract, input_csv_file_path) in input_csv_files:
@@ -41,6 +43,7 @@ for (header, columns_to_extract, input_csv_file_path) in input_csv_files:
 	csv_data_max_errors = numpy.sort(csv_data, order='max_errors')['max_errors']
 	csv_data_max_errors_by_raw_size = numpy.sort(csv_data, order='raw_sizes')['max_errors']
 	csv_data_ratio_by_max_error = numpy.sort(csv_data, order='max_errors')['compression_ratios']
+	csv_data_durations = numpy.sort(csv_data, order='durations')['durations']
 
 	output_csv_data_ratios.append(csv_data_ratios)
 	output_csv_data_ratios_by_raw_size.append(csv_data_ratios_by_raw_size)
@@ -48,6 +51,7 @@ for (header, columns_to_extract, input_csv_file_path) in input_csv_files:
 	output_csv_data_max_errors_by_raw_size.append(csv_data_max_errors_by_raw_size)
 	output_csv_data_ratio_vs_max_error.append(csv_data_max_errors)
 	output_csv_data_ratio_vs_max_error.append(csv_data_ratio_by_max_error)
+	output_csv_data_durations.append(csv_data_durations)
 
 	output_csv_headers.append(header)
 
@@ -56,6 +60,7 @@ output_csv_data_ratios_by_raw_size = numpy.column_stack(output_csv_data_ratios_b
 output_csv_data_max_errors = numpy.column_stack(output_csv_data_max_errors)
 output_csv_data_max_errors_by_raw_size = numpy.column_stack(output_csv_data_max_errors_by_raw_size)
 output_csv_data_ratio_vs_max_error = numpy.column_stack(output_csv_data_ratio_vs_max_error)
+output_csv_data_durations = numpy.column_stack(output_csv_data_durations)
 
 with open(output_csv_file_path_ratios, 'wb') as f:
 	header = bytes('{}\n'.format(','.join(output_csv_headers)), 'utf-8')
@@ -81,3 +86,8 @@ with open(output_csv_file_path_ratio_vs_max_error, 'wb') as f:
 	header = bytes('{}\n'.format(','.join(output_csv_headers)), 'utf-8')
 	f.write(header)
 	numpy.savetxt(f, output_csv_data_ratio_vs_max_error, delimiter=',', fmt=('%f'))
+
+with open(output_csv_file_path_durations, 'wb') as f:
+	header = bytes('{}\n'.format(','.join(output_csv_headers)), 'utf-8')
+	f.write(header)
+	numpy.savetxt(f, output_csv_data_durations, delimiter=',', fmt=('%f'))
