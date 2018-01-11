358c8d15f03eb911b84c3297ff28cd9b5cacaf1e
Nicholas Frechette <zeno490@gmail.com>
2018-01-10 22:20:38 -0500

    Adding missing include.

--- includes/acl/core/iallocator.h
+++ includes/acl/core/iallocator.h
@@ -25,6 +25,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #include "acl/core/error.h"
+#include "acl/core/memory_utils.h"
 
 #include <type_traits>
 #include <utility>
