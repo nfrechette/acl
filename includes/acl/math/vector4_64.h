8a7abe23991d024ee72e99979991662954d01ddd
Nicholas Frechette <zeno490@gmail.com>
2018-01-19 19:42:59 -0500

    Cleanup for consistency with Vector4_32. Fixed some bugs in the vector compare functions. Implemented a fallback impl for the unoptimized vector_mix variants.

--- includes/acl/math/vector4_64.h
+++ includes/acl/math/vector4_64.h
@@ -31,6 +31,9 @@
 
 namespace acl
 {
+	//////////////////////////////////////////////////////////////////////////
+	// Setters, getters, and casts
+
 	inline Vector4_64 vector_set(double x, double y, double z, double w)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
@@ -61,7 +64,14 @@ namespace acl
 
 	inline Vector4_64 vector_unaligned_load(const double* input)
 	{
+		ACL_ENSURE(is_aligned(input), "Invalid alignment");
 		return vector_set(input[0], input[1], input[2], input[3]);
+	}
+
+	inline Vector4_64 vector_unaligned_load3(const double* input)
+	{
+		ACL_ENSURE(is_aligned(input), "Invalid alignment");
+		return vector_set(input[0], input[1], input[2], 0.0);
 	}
 
 	inline Vector4_64 vector_unaligned_load_64(const uint8_t* input)
@@ -69,11 +79,6 @@ namespace acl
 		Vector4_64 result;
 		memcpy(&result, input, sizeof(Vector4_64));
 		return result;
-	}
-
-	inline Vector4_64 vector_unaligned_load3(const double* input)
-	{
-		return vector_set(input[0], input[1], input[2], 0.0);
 	}
 
 	inline Vector4_64 vector_unaligned_load3_64(const uint8_t* input)
@@ -179,19 +184,18 @@ namespace acl
 		}
 	}
 
-	inline double* vector_as_double_ptr(Vector4_64& input)
-	{
-		return reinterpret_cast<double*>(&input);
-	}
-
 	inline const double* vector_as_double_ptr(const Vector4_64& input)
 	{
 		return reinterpret_cast<const double*>(&input);
 	}
 
-	inline void vector_unaligned_write(const Vector4_64& input, uint8_t* output)
+	inline void vector_unaligned_write(const Vector4_64& input, double* output)
 	{
-		memcpy(output, &input, sizeof(Vector4_64));
+		ACL_ENSURE(is_aligned(output), "Invalid alignment");
+		output[0] = vector_get_x(input);
+		output[1] = vector_get_y(input);
+		output[2] = vector_get_z(input);
+		output[3] = vector_get_w(input);
 	}
 
 	inline void vector_unaligned_write3(const Vector4_64& input, double* output)
@@ -202,10 +206,18 @@ namespace acl
 		output[2] = vector_get_z(input);
 	}
 
+	inline void vector_unaligned_write(const Vector4_64& input, uint8_t* output)
+	{
+		memcpy(output, &input, sizeof(Vector4_64));
+	}
+
 	inline void vector_unaligned_write3(const Vector4_64& input, uint8_t* output)
 	{
 		memcpy(output, &input, sizeof(double) * 3);
 	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Arithmetic
 
 	inline Vector4_64 vector_add(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
@@ -317,31 +329,37 @@ namespace acl
 
 	inline double vector_length(const Vector4_64& input)
 	{
-		// TODO: Use intrinsics to avoid scalar coercion
 		return sqrt(vector_length_squared(input));
 	}
 
 	inline double vector_length3(const Vector4_64& input)
 	{
-		// TODO: Use intrinsics to avoid scalar coercion
 		return sqrt(vector_length_squared3(input));
 	}
 
 	inline double vector_length_reciprocal(const Vector4_64& input)
 	{
-		// TODO: Use recip instruction
 		return 1.0 / vector_length(input);
 	}
 
 	inline double vector_length_reciprocal3(const Vector4_64& input)
 	{
-		// TODO: Use recip instruction
 		return 1.0 / vector_length3(input);
 	}
 
 	inline double vector_distance3(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 		return vector_length3(vector_sub(rhs, lhs));
+	}
+
+	inline Vector4_64 vector_normalize3(const Vector4_64& input, double threshold = 0.00000001)
+	{
+		// Reciprocal is more accurate to normalize with
+		double inv_len = vector_length_reciprocal3(input);
+		if (inv_len >= threshold)
+			return vector_mul(input, vector_set(inv_len));
+		else
+			return input;
 	}
 
 	inline Vector4_64 vector_lerp(const Vector4_64& start, const Vector4_64& end, double alpha)
@@ -354,6 +372,21 @@ namespace acl
 		return vector_set(fraction(vector_get_x(input)), fraction(vector_get_y(input)), fraction(vector_get_z(input)), fraction(vector_get_w(input)));
 	}
 
+	// output = (input * scale) + offset
+	inline Vector4_64 vector_mul_add(const Vector4_64& input, const Vector4_64& scale, const Vector4_64& offset)
+	{
+		return vector_add(vector_mul(input, scale), offset);
+	}
+
+	// output = offset - (input * scale)
+	inline Vector4_64 vector_neg_mul_sub(const Vector4_64& input, const Vector4_64& scale, const Vector4_64& offset)
+	{
+		return vector_sub(offset, vector_mul(input, scale));
+	}
+
+	//////////////////////////////////////////////////////////////////////////
+	// Comparisons and masking
+
 	inline Vector4_64 vector_less_than(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
@@ -362,6 +395,17 @@ namespace acl
 		return Vector4_64{xy_lt_pd, zw_lt_pd};
 #else
 		return Vector4_64{math_impl::get_mask_value(lhs.x < rhs.x), math_impl::get_mask_value(lhs.y < rhs.y), math_impl::get_mask_value(lhs.z < rhs.z), math_impl::get_mask_value(lhs.w < rhs.w)};
+#endif
+	}
+
+	inline Vector4_64 vector_greater_equal(const Vector4_64& lhs, const Vector4_64& rhs)
+	{
+#if defined(ACL_SSE2_INTRINSICS)
+		__m128d xy_ge_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
+		__m128d zw_ge_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
+		return Vector4_64{ xy_ge_pd, zw_ge_pd };
+#else
+		return Vector4_32{ math_impl::get_mask_value(lhs.x >= rhs.x), math_impl::get_mask_value(lhs.y >= rhs.y), math_impl::get_mask_value(lhs.z >= rhs.z), math_impl::get_mask_value(lhs.w >= rhs.w) };
 #endif
 	}
 
@@ -412,9 +456,9 @@ namespace acl
 	inline bool vector_all_less_equal(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
-		return (_mm_movemask_pd(xy_lt_pd) & _mm_movemask_pd(zw_lt_pd)) == 3;
+		__m128d xy_le_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
+		__m128d zw_le_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
+		return (_mm_movemask_pd(xy_le_pd) & _mm_movemask_pd(zw_le_pd)) == 3;
 #else
 		return lhs.x <= rhs.x && lhs.y <= rhs.y && lhs.z <= rhs.z && lhs.w <= rhs.w;
 #endif
@@ -423,9 +467,9 @@ namespace acl
 	inline bool vector_all_less_equal3(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
-		return _mm_movemask_pd(xy_lt_pd) == 3 && (_mm_movemask_pd(zw_lt_pd) & 1) != 0;
+		__m128d xy_le_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
+		__m128d zw_le_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
+		return _mm_movemask_pd(xy_le_pd) == 3 && (_mm_movemask_pd(zw_le_pd) & 1) != 0;
 #else
 		return lhs.x <= rhs.x && lhs.y <= rhs.y && lhs.z <= rhs.z;
 #endif
@@ -434,9 +478,9 @@ namespace acl
 	inline bool vector_any_less_equal(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
-		return (_mm_movemask_pd(xy_lt_pd) | _mm_movemask_pd(zw_lt_pd)) != 0;
+		__m128d xy_le_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
+		__m128d zw_le_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
+		return (_mm_movemask_pd(xy_le_pd) | _mm_movemask_pd(zw_le_pd)) != 0;
 #else
 		return lhs.x <= rhs.x || lhs.y <= rhs.y || lhs.z <= rhs.z || lhs.w <= rhs.w;
 #endif
@@ -445,9 +489,9 @@ namespace acl
 	inline bool vector_any_less_equal3(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
-		return _mm_movemask_pd(xy_lt_pd) != 0 || _mm_movemask_pd(zw_lt_pd) != 2;
+		__m128d xy_le_pd = _mm_cmple_pd(lhs.xy, rhs.xy);
+		__m128d zw_le_pd = _mm_cmple_pd(lhs.zw, rhs.zw);
+		return _mm_movemask_pd(xy_le_pd) != 0 || (_mm_movemask_pd(zw_le_pd) & 1) != 0;
 #else
 		return lhs.x <= rhs.x || lhs.y <= rhs.y || lhs.z <= rhs.z;
 #endif
@@ -456,9 +500,9 @@ namespace acl
 	inline bool vector_all_greater_equal(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
-		return (_mm_movemask_pd(xy_lt_pd) & _mm_movemask_pd(zw_lt_pd)) == 3;
+		__m128d xy_ge_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
+		__m128d zw_ge_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
+		return (_mm_movemask_pd(xy_ge_pd) & _mm_movemask_pd(zw_ge_pd)) == 3;
 #else
 		return lhs.x >= rhs.x && lhs.y >= rhs.y && lhs.z >= rhs.z && lhs.w >= rhs.w;
 #endif
@@ -467,9 +511,9 @@ namespace acl
 	inline bool vector_all_greater_equal3(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
-		return _mm_movemask_pd(xy_lt_pd) == 3 && (_mm_movemask_pd(zw_lt_pd) & 1) != 0;
+		__m128d xy_ge_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
+		__m128d zw_ge_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
+		return _mm_movemask_pd(xy_ge_pd) == 3 && (_mm_movemask_pd(zw_ge_pd) & 1) != 0;
 #else
 		return lhs.x >= rhs.x && lhs.y >= rhs.y && lhs.z >= rhs.z;
 #endif
@@ -478,9 +522,9 @@ namespace acl
 	inline bool vector_any_greater_equal(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
-		return (_mm_movemask_pd(xy_lt_pd) & _mm_movemask_pd(zw_lt_pd)) != 0;
+		__m128d xy_ge_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
+		__m128d zw_ge_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
+		return (_mm_movemask_pd(xy_ge_pd) | _mm_movemask_pd(zw_ge_pd)) != 0;
 #else
 		return lhs.x >= rhs.x || lhs.y >= rhs.y || lhs.z >= rhs.z || lhs.w >= rhs.w;
 #endif
@@ -489,9 +533,9 @@ namespace acl
 	inline bool vector_any_greater_equal3(const Vector4_64& lhs, const Vector4_64& rhs)
 	{
 #if defined(ACL_SSE2_INTRINSICS)
-		__m128d xy_lt_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
-		__m128d zw_lt_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
-		return _mm_movemask_pd(xy_lt_pd) != 0 || _mm_movemask_pd(zw_lt_pd) != 2;
+		__m128d xy_ge_pd = _mm_cmpge_pd(lhs.xy, rhs.xy);
+		__m128d zw_ge_pd = _mm_cmpge_pd(lhs.zw, rhs.zw);
+		return _mm_movemask_pd(xy_ge_pd) != 0 || (_mm_movemask_pd(zw_ge_pd) & 1) != 0;
 #else
 		return lhs.x >= rhs.x || lhs.y >= rhs.y || lhs.z >= rhs.z;
 #endif
@@ -499,22 +543,22 @@ namespace acl
 
 	inline bool vector_all_near_equal(const Vector4_64& lhs, const Vector4_64& rhs, double threshold = 0.00001)
 	{
-		return vector_all_less_than(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
+		return vector_all_less_equal(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
 	}
 
 	inline bool vector_all_near_equal3(const Vector4_64& lhs, const Vector4_64& rhs, double threshold = 0.00001)
 	{
-		return vector_all_less_than3(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
+		return vector_all_less_equal3(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
 	}
 
 	inline bool vector_any_near_equal(const Vector4_64& lhs, const Vector4_64& rhs, double threshold = 0.00001)
 	{
-		return vector_any_less_than(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
+		return vector_any_less_equal(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
 	}
 
 	inline bool vector_any_near_equal3(const Vector4_64& lhs, const Vector4_64& rhs, double threshold = 0.00001)
 	{
-		return vector_any_less_than3(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
+		return vector_any_less_equal3(vector_abs(vector_sub(lhs, rhs)), vector_set(threshold));
 	}
 
 	inline bool vector_is_finite(const Vector4_64& input)
@@ -525,18 +569,6 @@ namespace acl
 	inline bool vector_is_finite3(const Vector4_64& input)
 	{
 		return is_finite(vector_get_x(input)) && is_finite(vector_get_y(input)) && is_finite(vector_get_z(input));
-	}
-
-	// output = (input * scale) + offset
-	inline Vector4_64 vector_mul_add(const Vector4_64& input, const Vector4_64& scale, const Vector4_64& offset)
-	{
-		return vector_add(vector_mul(input, scale), offset);
-	}
-
-	// output = offset - (input * scale)
-	inline Vector4_64 vector_neg_mul_sub(const Vector4_64& input, const Vector4_64& scale, const Vector4_64& offset)
-	{
-		return vector_sub(offset, vector_mul(input, scale));
 	}
 
 	//////////////////////////////////////////////////////////////////////////
@@ -604,8 +636,13 @@ namespace acl
 			return vector_set(vector_get_component(input1, comp0), vector_get_component(input0, comp1), vector_get_component(input1, comp2), vector_get_component(input0, comp3));
 		}
 
-		ACL_ENSURE(false, "vector_mix permutation not handled");
-		return input0;
+		// Slow code path, not yet optimized
+		//ACL_ENSURE(false, "vector_mix permutation not handled");
+		const double x = math_impl::is_vector_mix_arg_xyzw(comp0) ? vector_get_component<comp0>(input0) : vector_get_component<comp0>(input1);
+		const double y = math_impl::is_vector_mix_arg_xyzw(comp1) ? vector_get_component<comp1>(input0) : vector_get_component<comp1>(input1);
+		const double z = math_impl::is_vector_mix_arg_xyzw(comp2) ? vector_get_component<comp2>(input0) : vector_get_component<comp2>(input1);
+		const double w = math_impl::is_vector_mix_arg_xyzw(comp3) ? vector_get_component<comp3>(input0) : vector_get_component<comp3>(input1);
+		return vector_set(x, y, z, w);
 	}
 
 	inline Vector4_64 vector_mix_xxxx(const Vector4_64& input) { return vector_mix<VectorMix::X, VectorMix::X, VectorMix::X, VectorMix::X>(input, input); }
@@ -640,4 +677,13 @@ namespace acl
 	inline Vector4_64 vector_mix_bywx(const Vector4_64& input0, const Vector4_64& input1) { return vector_mix<VectorMix::B, VectorMix::Y, VectorMix::W, VectorMix::X>(input0, input1); }
 	inline Vector4_64 vector_mix_dxwc(const Vector4_64& input0, const Vector4_64& input1) { return vector_mix<VectorMix::D, VectorMix::X, VectorMix::W, VectorMix::C>(input0, input1); }
 	inline Vector4_64 vector_mix_dywx(const Vector4_64& input0, const Vector4_64& input1) { return vector_mix<VectorMix::D, VectorMix::Y, VectorMix::W, VectorMix::X>(input0, input1); }
+
+	//////////////////////////////////////////////////////////////////////////
+	// Misc
+
+	inline Vector4_64 vector_sign(const Vector4_64& input)
+	{
+		Vector4_64 mask = vector_greater_equal(input, vector_zero_64());
+		return vector_blend(mask, vector_set(1.0), vector_set(-1.0));
+	}
 }
