c6d53655a34424eed7d4d2a14f5d85ff709a790e
Nicholas Frechette <zeno490@gmail.com>
2018-02-19 18:32:45 -0500

    Fix win32 handling to clean up code a bit.

--- tests/sources/main.cpp
+++ tests/sources/main.cpp
@@ -25,29 +25,21 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
-#ifndef _WIN32
-static int _kbhit()
-{
-	return 0;
-}
-
-static bool IsDebuggerPresent()
-{
-	return false;
-}
-#else // _WIN32
+#ifdef _WIN32
 #include <conio.h>
-#endif // _WIN32
+#endif
 
 int main(int argc, char* argv[])
 {
 	int result = Catch::Session().run(argc, argv);
 
+#ifdef _WIN32
 	if (IsDebuggerPresent())
 	{
 		printf("Press any key to continue...\n");
 		while (_kbhit() == 0);
 	}
+#endif
 
 	return (result < 0xff ? result : 0xff);
 }
