a6185d0f1b1b4beabcedac8df4b76f64b4efd9ba
Nicholas Frechette <zeno490@gmail.com>
2018-02-28 23:03:14 -0500

    Refactored the cmake compiler flags into a macro. Renamed cmake file.

---
+++ cmake/CMakeCompiler.cmake
@@ -0,0 +1,47 @@
+cmake_minimum_required (VERSION 3.2)
+
+macro(setup_default_compiler_flags _project_name)
+	set(CMAKE_CXX_STANDARD 11)
+
+	if(MSVC)
+		# Replace some default compiler switches and add new ones
+		STRING(REPLACE "/GR" "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})		# Disable RTTI
+		STRING(REPLACE "/W3" "/W4" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})	# Bump warnings to W4
+		target_compile_options(${_project_name} PRIVATE /Zi)				# Add debug info
+		target_compile_options(${_project_name} PRIVATE /Oi)				# Generate intrinsic functions
+
+		# Disable some warnings
+		target_compile_options(${_project_name} PRIVATE /wd4100)			# unreferenced formal parameter
+		target_compile_options(${_project_name} PRIVATE /wd4324)			# structure was padded due to alignment specified
+		target_compile_options(${_project_name} PRIVATE /wd4127)			# conditional expression is constant
+
+		if(MSVC_VERSION GREATER 1900)
+			# VS2017 and above
+			target_compile_options(${_project_name} PRIVATE /permissive-)
+		endif()
+
+		if(USE_AVX_INSTRUCTIONS)
+			target_compile_options(${_project_name} PRIVATE "/arch:AVX")
+		endif()
+
+		# Add linker flags
+		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /DEBUG")
+	else()
+		# TODO: Handle OS X properly: https://stackoverflow.com/questions/5334095/cmake-multiarchitecture-compilation
+		if(CPU_INSTRUCTION_SET MATCHES "x86")
+			target_compile_options(${_project_name} PRIVATE "-m32")
+			target_link_libraries(${_project_name} PRIVATE "-m32")
+		elseif(CPU_INSTRUCTION_SET MATCHES "x64")
+			target_compile_options(${_project_name} PRIVATE "-m64")
+			target_link_libraries(${_project_name} PRIVATE "-m64")
+		endif()
+
+		if(CPU_INSTRUCTION_SET MATCHES "x86" OR CPU_INSTRUCTION_SET MATCHES "x64")
+			if(USE_AVX_INSTRUCTIONS)
+				target_compile_options(${_project_name} PRIVATE "-mavx")
+			else()
+				target_compile_options(${_project_name} PRIVATE "-msse4.1")
+			endif()
+		endif()
+	endif()
+endmacro()
