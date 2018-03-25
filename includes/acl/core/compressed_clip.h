286565990b819b928ac0018f045deadf4097320b
Nicholas Frechette <zeno490@gmail.com>
2018-03-25 10:32:48 -0700

    Commenting compressed clip and relevant structures/functions.

--- includes/acl/core/compressed_clip.h
+++ includes/acl/core/compressed_clip.h
@@ -35,12 +35,28 @@
 
 namespace acl
 {
+	////////////////////////////////////////////////////////////////////////////////
+	// An instance of a compressed clip.
+	// The compressed data immediately follows the clip instance.
+	////////////////////////////////////////////////////////////////////////////////
 	class alignas(16) CompressedClip
 	{
 	public:
+		////////////////////////////////////////////////////////////////////////////////
+		// Returns the algorithm type used to compress the clip.
 		AlgorithmType8 get_algorithm_type() const { return m_type; }
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Returns the size in bytes of the compressed clip.
+		// Includes the 'CompressedClip' instance size.
 		uint32_t get_size() const { return m_size; }
 
+		////////////////////////////////////////////////////////////////////////////////
+		// Returns true if a compressed clip is valid and usable.
+		// This mainly validates some invariants as well as ensuring that the
+		// memory has not been corrupted.
+		//
+		// check_hash: If true, the compressed clip hash will also be compared.
 		bool is_valid(bool check_hash) const
 		{
 			if (!is_aligned_to(this, alignof(CompressedClip)))
@@ -65,9 +81,16 @@ namespace acl
 		}
 
 	private:
+		////////////////////////////////////////////////////////////////////////////////
+		// A known tag value to distinguis compressed clips from other things.
 		static constexpr uint32_t k_compressed_clip_tag = 0xac10ac10;
+
+		////////////////////////////////////////////////////////////////////////////////
+		// The number of bytes to skip in the header when calculating the hash.
 		static constexpr uint32_t k_hash_skip_size = sizeof(uint32_t) + sizeof(uint32_t);	// m_size + m_hash
 
+		////////////////////////////////////////////////////////////////////////////////
+		// Constructs a compressed clip instance
 		CompressedClip(uint32_t size, AlgorithmType8 type)
 			: m_size(size)
 			, m_hash(hash32(safe_ptr_cast<const uint8_t>(this) + k_hash_skip_size, size - k_hash_skip_size))
@@ -79,58 +102,112 @@ namespace acl
 			(void)m_padding;	// Avoid unused warning
 		}
 
-		// 16 byte header, the rest of the data follows in memory
+		////////////////////////////////////////////////////////////////////////////////
+		// 16 byte header, the rest of the data follows in memory.
+		////////////////////////////////////////////////////////////////////////////////
+
+		// Total size in bytes of the compressed clip. Includes 'sizeof(CompressedClip)'.
 		uint32_t		m_size;
+
+		// Hash of the compressed clip. Hashed memory starts immediatly after this.
 		uint32_t		m_hash;
 
-		// Everything starting here is included in the hash
+		////////////////////////////////////////////////////////////////////////////////
+		// Everything starting here is included in the hash.
+		////////////////////////////////////////////////////////////////////////////////
+
+		// Serialization tag used to distinguish raw buffer types.
 		uint32_t		m_tag;
+
+		// Serialization version used to compress the clip.
 		uint16_t		m_version;
+
+		// Algorithm type used to compress the clip.
 		AlgorithmType8	m_type;
+
+		// Unused memory left as padding
 		uint8_t			m_padding;
 
+		////////////////////////////////////////////////////////////////////////////////
+		// Friend function used to construct compressed clip instances. Should only
+		// be called by encoders.
 		friend CompressedClip* make_compressed_clip(void* buffer, uint32_t size, AlgorithmType8 type);
+
+		////////////////////////////////////////////////////////////////////////////////
+		// Friend function to finalize a compressed clip once all memory has been written within.
 		friend void finalize_compressed_clip(CompressedClip& compressed_clip);
 	};
 
 	static_assert(alignof(CompressedClip) == 16, "Invalid alignment for CompressedClip");
 	static_assert(sizeof(CompressedClip) == 16, "Invalid size for CompressedClip");
 
+	////////////////////////////////////////////////////////////////////////////////
+	// A compressed clip segment header. Each segment is built from a uniform number
+	// of samples per track. A clip is split into one or more segments.
+	////////////////////////////////////////////////////////////////////////////////
 	struct SegmentHeader
 	{
+		// Number of samples the segment was constructed from per track.
 		uint32_t				num_samples;
+
+		// Number of bits used by a fully animated pose (excludes default/constant tracks).
 		uint32_t				animated_pose_bit_size;						// TODO: Calculate from bitsets and formats?
 
-																			// TODO: Only need one offset, calculate the others from the information we have?
+		// TODO: Only need one offset, calculate the others from the information we have?
+		// Offset to the per animated track format data.
 		PtrOffset32<uint8_t>	format_per_track_data_offset;				// TODO: Make this offset optional? Only present if variable
+
+		// Offset to the segment range data.
 		PtrOffset32<uint8_t>	range_data_offset;							// TODO: Make this offset optional? Only present if normalized
+
+		// Offset to the segment animated tracks data.
 		PtrOffset32<uint8_t>	track_data_offset;
 	};
 
+	////////////////////////////////////////////////////////////////////////////////
+	// A compressed clip header.
+	////////////////////////////////////////////////////////////////////////////////
 	struct ClipHeader
 	{
+		// The number of bones compressed.
 		uint16_t				num_bones;
+
+		// The number of segments contained.
 		uint16_t				num_segments;
 
+		// The rotation/translation/scale format used.
 		RotationFormat8			rotation_format;
 		VectorFormat8			translation_format;
 		VectorFormat8			scale_format;								// TODO: Make this optional?
 
+		// The clip/segment range reduction format used.
 		RangeReductionFlags8	clip_range_reduction;
 		RangeReductionFlags8	segment_range_reduction;
 
+		// Whether or not we have scale (bool).
 		uint8_t					has_scale;
 
+		// The total number of samples per track our clip contained.
 		uint32_t				num_samples;
+
+		// The clip sample rate.
 		uint32_t				sample_rate;								// TODO: Store duration as float instead
 
+		// Offset to the segment headers data.
 		PtrOffset16<SegmentHeader>	segment_headers_offset;
+
+		// Offsets to the default/constant tracks bitsets.
 		PtrOffset16<uint32_t>		default_tracks_bitset_offset;
 		PtrOffset16<uint32_t>		constant_tracks_bitset_offset;
+
+		// Offset to the constant tracks data.
 		PtrOffset16<uint8_t>		constant_track_data_offset;
+
+		// Offset to the clip range data.
 		PtrOffset16<uint8_t>		clip_range_data_offset;					// TODO: Make this offset optional? Only present if normalized
 
 		//////////////////////////////////////////////////////////////////////////
+		// Utility functions that return pointers from their respective offsets.
 
 		SegmentHeader*			get_segment_headers()		{ return segment_headers_offset.add_to(this); }
 		const SegmentHeader*	get_segment_headers() const	{ return segment_headers_offset.add_to(this); }
@@ -157,11 +234,13 @@ namespace acl
 		const uint8_t*	get_segment_range_data(const SegmentHeader& header) const	{ return header.range_data_offset.safe_add_to(this); }
 	};
 
+	// Returns the clip header for a compressed clip.
 	inline ClipHeader& get_clip_header(CompressedClip& clip)
 	{
 		return *add_offset_to_ptr<ClipHeader>(&clip, sizeof(CompressedClip));
 	}
 
+	// Returns the clip header for a compressed clip.
 	inline const ClipHeader& get_clip_header(const CompressedClip& clip)
 	{
 		return *add_offset_to_ptr<const ClipHeader>(&clip, sizeof(CompressedClip));
