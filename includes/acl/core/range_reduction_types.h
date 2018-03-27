a8b49eb307d038a1a6ef02c054aa9c28f035612a
Nicholas Frechette <zeno490@gmail.com>
2018-03-26 20:43:00 -0400

    Commented range reduction related functions.

--- includes/acl/core/range_reduction_types.h
+++ includes/acl/core/range_reduction_types.h
@@ -30,10 +30,15 @@
 
 namespace acl
 {
+	////////////////////////////////////////////////////////////////////////////////
+	// Various constants for range reduction.
 	constexpr uint8_t k_segment_range_reduction_num_bits_per_component = 8;
 	constexpr uint8_t k_segment_range_reduction_num_bytes_per_component = 1;
 	constexpr uint32_t k_clip_range_reduction_vector3_range_size = sizeof(float) * 6;
 
+	////////////////////////////////////////////////////////////////////////////////
+	// RangeReductionFlags8 represents the types of range reduction we support as a bit field.
+	//
 	// BE CAREFUL WHEN CHANGING VALUES IN THIS ENUM
 	// The range reduction strategy is serialized in the compressed data, if you change a value
 	// the compressed clips will be invalid. If you do, bump the appropriate algorithm versions.
@@ -52,6 +57,8 @@ namespace acl
 
 	//////////////////////////////////////////////////////////////////////////
 
+	////////////////////////////////////////////////////////////////////////////////
+	// Returns a string of the algorithm name suitable for display.
 	// TODO: constexpr
 	inline const char* get_range_reduction_name(RangeReductionFlags8 flags)
 	{
