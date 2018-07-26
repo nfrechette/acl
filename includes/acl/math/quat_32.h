0da32e11d958d6383d09b963e3c97eec9abfd2e1
Nicholas Frechette <zeno490@gmail.com>
2018-07-25 21:19:48 -0400

    Optimize quat_from_positive_w on SSE2/SSE4. Fixes #134.

--- includes/acl/math/quat_32.h
+++ includes/acl/math/quat_32.h
@@ -431,12 +431,37 @@ namespace acl
 
 	inline Quat_32 quat_from_positive_w(const Vector4_32& input)
 	{
+#if defined(ACL_SSE4_INTRINSICS)
+		__m128 x2y2z2 = _mm_mul_ps(input, input);
+		__m128 one = _mm_set_ss(1.0f);
+		__m128 w_squared = _mm_sub_ss(_mm_sub_ss(_mm_sub_ss(one, x2y2z2), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(1, 1, 1, 1))), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(2, 2, 2, 2)));
+		w_squared = _mm_andnot_ps(_mm_set_ss(-0.0f), w_squared);
+		__m128 w = _mm_sqrt_ss(w_squared);
+		return _mm_insert_ps(input, w, 0x30);
+#elif defined(ACL_SSE2_INTRINSICS)
+		__m128 x2y2z2 = _mm_mul_ps(input, input);
+		__m128 one = _mm_set_ss(1.0f);
+		__m128 w_squared = _mm_sub_ss(_mm_sub_ss(_mm_sub_ss(one, x2y2z2), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(1, 1, 1, 1))), _mm_shuffle_ps(x2y2z2, x2y2z2, _MM_SHUFFLE(2, 2, 2, 2)));
+		w_squared = _mm_andnot_ps(_mm_set_ss(-0.0f), w_squared);
+		__m128 w = _mm_sqrt_ss(w_squared);
+		__m128 input_wyzx = _mm_shuffle_ps(input, input, _MM_SHUFFLE(0, 2, 1, 3));
+		__m128 result_wyzx = _mm_move_ss(input_wyzx, w);
+		return _mm_shuffle_ps(result_wyzx, result_wyzx, _MM_SHUFFLE(0, 2, 1, 3));
+#elif defined(ACL_NEON_INTRINSICS) && 0
+		// TODO: This is slower on ARMv7-A, measure again on ARM64, fewer instructions but the first
+		// sub is dependent on the result of the mul where the C impl below pipelines a bit better it seems
+		float32x4_t x2y2z2 = vmulq_f32(input, input);
+		float w_squared = ((1.0f - vgetq_lane_f32(x2y2z2, 0)) - vgetq_lane_f32(x2y2z2, 1)) - vgetq_lane_f32(x2y2z2, 2);
+		float w = acl::sqrt(acl::abs(w_squared));
+		return vsetq_lane_f32(w, input, 3);
+#else
 		// Operation order is important here, due to rounding, ((1.0 - (X*X)) - Y*Y) - Z*Z is more accurate than 1.0 - dot3(xyz, xyz)
 		float w_squared = ((1.0f - vector_get_x(input) * vector_get_x(input)) - vector_get_y(input) * vector_get_y(input)) - vector_get_z(input) * vector_get_z(input);
 		// w_squared can be negative either due to rounding or due to quantization imprecision, we take the absolute value
 		// to ensure the resulting quaternion is always normalized with a positive W component
 		float w = sqrt(abs(w_squared));
 		return quat_set(vector_get_x(input), vector_get_y(input), vector_get_z(input), w);
+#endif
 	}
 
 	//////////////////////////////////////////////////////////////////////////
