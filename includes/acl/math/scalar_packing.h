6f73ffe9bc3363e6526443e431545f92a3d7b0b2
Nicholas Frechette <zeno490@gmail.com>
2018-08-12 14:46:24 -0400

    Changing asserts a bit.

--- includes/acl/math/scalar_packing.h
+++ includes/acl/math/scalar_packing.h
@@ -33,6 +33,7 @@ namespace acl
 {
 	inline uint32_t pack_scalar_unsigned(float input, uint8_t num_bits)
 	{
+		ACL_ASSERT(num_bits < 31, "Attempting to pack on too many bits");
 		ACL_ASSERT(input >= 0.0f && input <= 1.0f, "Expected normalized unsigned input value: %f", input);
 		const uint32_t max_value = (1 << num_bits) - 1;
 		return static_cast<uint32_t>(symmetric_round(input * safe_to_float(max_value)));
@@ -40,6 +41,7 @@ namespace acl
 
 	inline float unpack_scalar_unsigned(uint32_t input, uint8_t num_bits)
 	{
+		ACL_ASSERT(num_bits < 31, "Attempting to unpack from too many bits");
 		const uint32_t max_value = (1 << num_bits) - 1;
 		ACL_ASSERT(input <= max_value, "Input value too large: %ull", input);
 		return safe_to_float(input) / safe_to_float(max_value);
@@ -47,7 +49,6 @@ namespace acl
 
 	inline uint32_t pack_scalar_signed(float input, uint8_t num_bits)
 	{
-		ACL_ASSERT(input >= -1.0f && input <= 1.0f, "Expected normalized signed input value: %f", input);
 		return pack_scalar_unsigned((input * 0.5f) + 0.5f, num_bits);
 	}
 
