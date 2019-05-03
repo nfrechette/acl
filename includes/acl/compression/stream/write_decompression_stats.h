33e2811cb0bab4ce125fac2faa3fe3f755794ff3
Nicholas Frechette <zeno490@gmail.com>
2019-05-02 23:12:20 -0400

    fix(stats): rework how decompression performance is measured
    The old method did not do enough work in between measurements,
    yielding a lot of noise and innaccurate results, especially on fast processors.
    Because we sampled every frame, short clips would have very high
    variance (e.g. static pose).
    The new method performs a fixed number of samples per clip regardless
    of its length. We also decompress a large number of duplicate copies
    in order to better approximate the time taken.
    Some noise remains with the cold CPU cache due to memory bus/cache
    contention but the warm CPU cache measurements are very accurate now.
    
    Fixes #191

--- includes/acl/compression/stream/write_decompression_stats.h
+++ includes/acl/compression/stream/write_decompression_stats.h
@@ -34,141 +34,154 @@
 #include "acl/compression/output_stats.h"
 #include "acl/decompression/default_output_writer.h"
 
+#include <algorithm>
 #include <thread>
 #include <chrono>
+#include <cstring>
+#include <random>
 
 ACL_IMPL_FILE_PRAGMA_PUSH
 
 namespace acl
 {
-	constexpr uint32_t k_num_decompression_timing_passes = 3;
+	constexpr uint32_t k_num_decompression_samples = 100;
+	constexpr uint32_t k_num_decompression_evaluations = 100;
 
-	enum class PlaybackDirection8 : uint8_t
+	enum class PlaybackDirection
 	{
 		Forward,
 		Backward,
 		Random,
 	};
 
-	enum class DecompressionFunction8 : uint8_t
+	enum class DecompressionFunction
 	{
 		DecompressPose,
 		DecompressBone,
 	};
 
 	template<class DecompressionContextType>
-	inline void write_decompression_performance_stats(const CompressedClip& compressed_clip,
+	inline void write_decompression_performance_stats(
 		StatLogging logging, sjson::ObjectWriter& writer, const char* action_type,
-		PlaybackDirection8 playback_direction, DecompressionFunction8 decompression_function,
-		DecompressionContextType* contexts[k_num_decompression_timing_passes], CPUCacheFlusher* cache_flusher, Transform_32* lossy_pose_transforms)
+		PlaybackDirection playback_direction, DecompressionFunction decompression_function,
+		CompressedClip* compressed_clips[k_num_decompression_evaluations],
+		DecompressionContextType* contexts[k_num_decompression_evaluations],
+		CPUCacheFlusher* cache_flusher, Transform_32* lossy_pose_transforms)
 	{
-		const ClipHeader& clip_header = get_clip_header(compressed_clip);
-		const int32_t num_samples = static_cast<int32_t>(clip_header.num_samples);
-		const float sample_rate = clip_header.sample_rate;
-		const float duration = calculate_duration(num_samples, clip_header.sample_rate);
+		const ClipHeader& clip_header = get_clip_header(*compressed_clips[0]);
+		const float duration = calculate_duration(clip_header.num_samples, clip_header.sample_rate);
 		const uint16_t num_bones = clip_header.num_bones;
+		const bool is_cold_cache_profiling = cache_flusher != nullptr;
+
+		float sample_times[k_num_decompression_samples];
+		for (uint32_t sample_index = 0; sample_index < k_num_decompression_samples; ++sample_index)
+		{
+			const float normalized_sample_time = float(sample_index) / float(k_num_decompression_samples - 1);
+			sample_times[sample_index] = clamp(normalized_sample_time, 0.0f, 1.0f) * duration;
+		}
+
+		switch (playback_direction)
+		{
+		case PlaybackDirection::Forward:
+		default:
+			break;
+		case PlaybackDirection::Backward:
+			std::reverse(&sample_times[0], &sample_times[k_num_decompression_samples]);
+			break;
+		case PlaybackDirection::Random:
+			std::shuffle(&sample_times[0], &sample_times[k_num_decompression_samples], std::default_random_engine(0));
+			break;
+		}
 
 		DefaultOutputWriter pose_writer(lossy_pose_transforms, num_bones);
 
-		for (uint32_t pass_index = 0; pass_index < k_num_decompression_timing_passes; ++pass_index)
-			contexts[pass_index]->initialize(compressed_clip);
+		// Initialize and clear our contexts
+		for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+			contexts[clip_index]->initialize(*compressed_clips[clip_index]);
 
 		writer[action_type] = [&](sjson::ObjectWriter& action_writer)
 		{
-			int32_t initial_sample_index;
-			int32_t sample_index_sentinel;
-			int32_t delta_sample_index;
-
-			switch (playback_direction)
-			{
-			case PlaybackDirection8::Forward:
-			case PlaybackDirection8::Random:
-			default:
-				initial_sample_index = 0;
-				sample_index_sentinel = num_samples;
-				delta_sample_index = 1;
-				break;
-			case PlaybackDirection8::Backward:
-				initial_sample_index = num_samples - 1;
-				sample_index_sentinel = -1;
-				delta_sample_index = -1;
-				break;
-			}
 
 			double clip_max_ms = 0.0;
 			double clip_min_ms = 1000000.0;
 			double clip_total_ms = 0.0;
+			double clip_time_ms[k_num_decompression_samples];
 
 			action_writer["data"] = [&](sjson::ArrayWriter& data_writer)
 			{
-				for (int32_t sample_index = initial_sample_index; sample_index != sample_index_sentinel; sample_index += delta_sample_index)
+				for (uint32_t sample_index = 0; sample_index < k_num_decompression_samples; ++sample_index)
 				{
-					const float sample_time = min(float(sample_index) / sample_rate, duration);
+					const float sample_time = sample_times[sample_index];
 
-					double decompression_time_ms = 1000000.0;
-
-					for (uint32_t pass_index = 0; pass_index < k_num_decompression_timing_passes; ++pass_index)
+					// Clearing the context ensures the decoder cannot reuse any state cached from the last sample.
+					if (playback_direction == PlaybackDirection::Random)
 					{
-						DecompressionContextType* context = contexts[pass_index];
+						for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+							contexts[clip_index]->initialize(*compressed_clips[clip_index]);
+					}
 
-						// Clearing the context ensures the decoder cannot reuse any state cached from the last sample.
-						if (playback_direction == PlaybackDirection8::Random)
-							context->initialize(compressed_clip);
-
-						if (cache_flusher != nullptr)
+					// Clear the CPU cache if necessary
+					if (is_cold_cache_profiling)
+					{
+						cache_flusher->begin_flushing();
+						for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
 						{
-							cache_flusher->begin_flushing();
-							cache_flusher->flush_buffer(context, sizeof(DecompressionContextType));
-							cache_flusher->flush_buffer(&compressed_clip, compressed_clip.get_size());
-							cache_flusher->end_flushing();
-
-							// Now that the cache is cold, yield our time slice and wait for a new one
-							// This helps minimize the risk that we'll be interrupted during decompression
-							std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+							cache_flusher->flush_buffer(contexts[clip_index], sizeof(DecompressionContextType));
+							cache_flusher->flush_buffer(compressed_clips[clip_index], compressed_clips[clip_index]->get_size());
 						}
-						else
-						{
-							// If we want the cache warm, decompress everything once to prime it
+						cache_flusher->end_flushing();
 
-							// We yield our time slice and wait for a new one before priming the cache
-							// to help keep it warm and minimize the risk that we'll be interrupted during decompression
-							std::this_thread::sleep_for(std::chrono::nanoseconds(1));
+						// If we want the cache warm, decompress everything once to prime it
+						DecompressionContextType* context = contexts[0];
+						context->seek(sample_time, SampleRoundingPolicy::None);
+						context->decompress_pose(pose_writer);
+					}
 
-							context->seek(sample_time, SampleRoundingPolicy::None);
-							context->decompress_pose(pose_writer);
-						}
+					// We yield our time slice and wait for a new one before priming the cache
+					// to help keep it warm and minimize the risk that we'll be interrupted during decompression
+					std::this_thread::sleep_for(std::chrono::nanoseconds(1));
 
-						ScopeProfiler timer;
+					ScopeProfiler timer;
+
+					for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+					{
+						// If we measure with a cold CPU cache, we use a different context every time otherwise we use the first one
+						DecompressionContextType* context = is_cold_cache_profiling ? contexts[clip_index] : contexts[0];
+
 						context->seek(sample_time, SampleRoundingPolicy::None);
 
 						switch (decompression_function)
 						{
-						case DecompressionFunction8::DecompressPose:
+						case DecompressionFunction::DecompressPose:
 							context->decompress_pose(pose_writer);
 							break;
-						case DecompressionFunction8::DecompressBone:
+						case DecompressionFunction::DecompressBone:
 							for (uint16_t bone_index = 0; bone_index < num_bones; ++bone_index)
 								context->decompress_bone(bone_index, &lossy_pose_transforms[bone_index].rotation, &lossy_pose_transforms[bone_index].translation, &lossy_pose_transforms[bone_index].scale);
 							break;
 						}
-						timer.stop();
-
-						const double elapsed_ms = timer.get_elapsed_milliseconds();
-						decompression_time_ms = min(decompression_time_ms, elapsed_ms);
 					}
 
-					if (are_any_enum_flags_set(logging, StatLogging::ExhaustiveDecompression))
-						data_writer.push(decompression_time_ms);
+					timer.stop();
 
-					clip_max_ms = max(clip_max_ms, decompression_time_ms);
-					clip_min_ms = min(clip_min_ms, decompression_time_ms);
-					clip_total_ms += decompression_time_ms;
+					const double elapsed_ms = timer.get_elapsed_milliseconds() / k_num_decompression_evaluations;
+
+					if (are_any_enum_flags_set(logging, StatLogging::ExhaustiveDecompression))
+						data_writer.push(elapsed_ms);
+
+					clip_min_ms = min(clip_min_ms, elapsed_ms);
+					clip_max_ms = max(clip_max_ms, elapsed_ms);
+					clip_total_ms += elapsed_ms;
+					clip_time_ms[sample_index] = elapsed_ms;
 				}
 			};
 
+			std::sort(&clip_time_ms[0], &clip_time_ms[k_num_decompression_samples]);
+
 			action_writer["min_time_ms"] = clip_min_ms;
 			action_writer["max_time_ms"] = clip_max_ms;
-			action_writer["avg_time_ms"] = clip_total_ms / double(num_samples);
+			action_writer["avg_time_ms"] = clip_total_ms / double(k_num_decompression_samples);
+			action_writer["med_time_ms"] = clip_time_ms[k_num_decompression_samples / 2];
 		};
 	}
 
@@ -177,7 +190,7 @@ namespace acl
 		Transform_32* memcpy_src_transforms = allocate_type_array<Transform_32>(allocator, num_bones);
 
 		double decompression_time_ms = 1000000.0;
-		for (uint32_t pass_index = 0; pass_index < k_num_decompression_timing_passes; ++pass_index)
+		for (uint32_t pass_index = 0; pass_index < 3; ++pass_index)
 		{
 			if (cache_flusher != nullptr)
 			{
@@ -238,11 +251,11 @@ namespace acl
 	}
 
 	template<class DecompressionContextType>
-	inline void write_decompression_performance_stats(IAllocator& allocator, const CompressedClip& compressed_clip, DecompressionContextType* contexts[k_num_decompression_timing_passes], StatLogging logging, sjson::ObjectWriter& writer)
+	inline void write_decompression_performance_stats(IAllocator& allocator, CompressedClip* compressed_clips[k_num_decompression_evaluations], DecompressionContextType* contexts[k_num_decompression_evaluations], StatLogging logging, sjson::ObjectWriter& writer)
 	{
 		CPUCacheFlusher* cache_flusher = allocate_type<CPUCacheFlusher>(allocator);
 
-		const ClipHeader& clip_header = get_clip_header(compressed_clip);
+		const ClipHeader& clip_header = get_clip_header(*compressed_clips[0]);
 		Transform_32* lossy_pose_transforms = allocate_type_array<Transform_32>(allocator, clip_header.num_bones);
 
 		const uint32_t num_bytes_per_bone = (4 + 3 + 3) * sizeof(float);	// Rotation, Translation, Scale
@@ -253,22 +266,26 @@ namespace acl
 			// Cold/Warm CPU cache, memcpy
 			write_memcpy_performance_stats(allocator, per_sample_writer, cache_flusher, lossy_pose_transforms, clip_header.num_bones);
 			write_memcpy_performance_stats(allocator, per_sample_writer, nullptr, lossy_pose_transforms, clip_header.num_bones);
+
 			// Cold CPU cache, decompress_pose
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "forward_pose_cold", PlaybackDirection8::Forward, DecompressionFunction8::DecompressPose, contexts, cache_flusher, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "backward_pose_cold", PlaybackDirection8::Backward, DecompressionFunction8::DecompressPose, contexts, cache_flusher, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "random_pose_cold", PlaybackDirection8::Random, DecompressionFunction8::DecompressPose, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "forward_pose_cold", PlaybackDirection::Forward, DecompressionFunction::DecompressPose, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "backward_pose_cold", PlaybackDirection::Backward, DecompressionFunction::DecompressPose, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "random_pose_cold", PlaybackDirection::Random, DecompressionFunction::DecompressPose, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+
 			// Warm CPU cache, decompress_pose
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "forward_pose_warm", PlaybackDirection8::Forward, DecompressionFunction8::DecompressPose, contexts, nullptr, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "backward_pose_warm", PlaybackDirection8::Backward, DecompressionFunction8::DecompressPose, contexts, nullptr, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "random_pose_warm", PlaybackDirection8::Random, DecompressionFunction8::DecompressPose, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "forward_pose_warm", PlaybackDirection::Forward, DecompressionFunction::DecompressPose, compressed_clips, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "backward_pose_warm", PlaybackDirection::Backward, DecompressionFunction::DecompressPose, compressed_clips, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "random_pose_warm", PlaybackDirection::Random, DecompressionFunction::DecompressPose, compressed_clips, contexts, nullptr, lossy_pose_transforms);
+
 			// Cold CPU cache, decompress_bone
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "forward_bone_cold", PlaybackDirection8::Forward, DecompressionFunction8::DecompressBone, contexts, cache_flusher, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "backward_bone_cold", PlaybackDirection8::Backward, DecompressionFunction8::DecompressBone, contexts, cache_flusher, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "random_bone_cold", PlaybackDirection8::Random, DecompressionFunction8::DecompressBone, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "forward_bone_cold", PlaybackDirection::Forward, DecompressionFunction::DecompressBone, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "backward_bone_cold", PlaybackDirection::Backward, DecompressionFunction::DecompressBone, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "random_bone_cold", PlaybackDirection::Random, DecompressionFunction::DecompressBone, compressed_clips, contexts, cache_flusher, lossy_pose_transforms);
+
 			// Warm CPU cache, decompress_bone
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "forward_bone_warm", PlaybackDirection8::Forward, DecompressionFunction8::DecompressBone, contexts, nullptr, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "backward_bone_warm", PlaybackDirection8::Backward, DecompressionFunction8::DecompressBone, contexts, nullptr, lossy_pose_transforms);
-			write_decompression_performance_stats(compressed_clip, logging, per_sample_writer, "random_bone_warm", PlaybackDirection8::Random, DecompressionFunction8::DecompressBone, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "forward_bone_warm", PlaybackDirection::Forward, DecompressionFunction::DecompressBone, compressed_clips, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "backward_bone_warm", PlaybackDirection::Backward, DecompressionFunction::DecompressBone, compressed_clips, contexts, nullptr, lossy_pose_transforms);
+			write_decompression_performance_stats(logging, per_sample_writer, "random_bone_warm", PlaybackDirection::Random, DecompressionFunction::DecompressBone, compressed_clips, contexts, nullptr, lossy_pose_transforms);
 		};
 
 		deallocate_type_array(allocator, lossy_pose_transforms, clip_header.num_bones);
@@ -296,14 +313,25 @@ namespace acl
 			ACL_ASSERT(use_uniform_fast_path, "We do not support profiling the debug code path");
 #endif
 
-			uniformly_sampled::DecompressionContext<uniformly_sampled::DefaultDecompressionSettings>* contexts[k_num_decompression_timing_passes];
-			for (uint32_t pass_index = 0; pass_index < k_num_decompression_timing_passes; ++pass_index)
-				contexts[pass_index] = uniformly_sampled::make_decompression_context<uniformly_sampled::DefaultDecompressionSettings>(allocator);
+			CompressedClip* compressed_clips[k_num_decompression_evaluations];
+			for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+			{
+				void* clip = allocator.allocate(compressed_clip.get_size(), alignof(CompressedClip));
+				std::memcpy(clip, &compressed_clip, compressed_clip.get_size());
+				compressed_clips[clip_index] = reinterpret_cast<CompressedClip*>(clip);
+			}
 
-			write_decompression_performance_stats(allocator, compressed_clip, contexts, logging, writer);
+			uniformly_sampled::DecompressionContext<uniformly_sampled::DefaultDecompressionSettings>* contexts[k_num_decompression_evaluations];
+			for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+				contexts[clip_index] = uniformly_sampled::make_decompression_context<uniformly_sampled::DefaultDecompressionSettings>(allocator);
 
-			for (uint32_t pass_index = 0; pass_index < k_num_decompression_timing_passes; ++pass_index)
+			write_decompression_performance_stats(allocator, compressed_clips, contexts, logging, writer);
+
+			for (uint32_t pass_index = 0; pass_index < k_num_decompression_evaluations; ++pass_index)
 				contexts[pass_index]->release();
+
+			for (uint32_t clip_index = 0; clip_index < k_num_decompression_evaluations; ++clip_index)
+				allocator.deallocate(compressed_clips[clip_index], compressed_clip.get_size());
 			break;
 		}
 		}
