7679bc0449bb4c4c037cca7852e85b58f77109f8
Nicholas Frechette <zeno490@gmail.com>
2019-05-11 11:35:26 -0400

    perf(android): enable all processors when building android

--- cmake/Toolchain-Android.cmake
+++ cmake/Toolchain-Android.cmake
@@ -4,3 +4,6 @@ set(CMAKE_SYSTEM_NAME Android)
 
 # Use the clang tool set because it's support for C++11 is superior
 set(CMAKE_GENERATOR_TOOLSET DefaultClang)
+
+# Make sure we use all our processors when building
+set(CMAKE_ANDROID_PROCESS_MAX $ENV{NUMBER_OF_PROCESSORS})
