39541008b60453f5dc3fbbf8ce478d12dbd9469d
Nicholas Frechette <zeno490@gmail.com>
2019-10-28 23:32:48 -0400

    fix(compression): fix and optimize local bit rate optimization pass
    We previously had multiple nested loops which exhibits poor branching and we
    also sometimes failed to minimize the memory footprint. We now exhaustively
    search with every permutation until we find one that satisfies our error threshold.
    
    Fixes #106

---
+++ tools/calc_local_bit_rates.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+
+import sys
+
+k_bit_rate_num_bits = [ 0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 32 ]
+
+k_highest_bit_rate = 18
+k_lowest_bit_rate = 1
+k_num_bit_rates = 19
+k_invalid_bit_rate = 255
+
+# This code assumes that rotations, translations, and scales are packed on 3 components (e.g. quat drop w)
+
+if __name__ == "__main__":
+	permutation_tries = []
+	permutation_tries_no_scale = []
+
+	for rotation_bit_rate in range(k_num_bit_rates):
+		for translation_bit_rate in range(k_num_bit_rates):
+			transform_size = k_bit_rate_num_bits[rotation_bit_rate] * 3 + k_bit_rate_num_bits[translation_bit_rate] * 3
+			permutation_tries_no_scale.append((transform_size, rotation_bit_rate, translation_bit_rate))
+
+			for scale_bit_rate in range(k_num_bit_rates):
+				transform_size = k_bit_rate_num_bits[rotation_bit_rate] * 3 + k_bit_rate_num_bits[translation_bit_rate] * 3 + k_bit_rate_num_bits[scale_bit_rate] * 3
+				permutation_tries.append((transform_size, rotation_bit_rate, translation_bit_rate, scale_bit_rate))
+
+	# Sort by transform size, then by each bit rate
+	permutation_tries.sort()
+	permutation_tries_no_scale.sort()
+
+	print('constexpr uint8_t k_local_bit_rate_permutations_no_scale[{}][2] ='.format(len(permutation_tries_no_scale)))
+	print('{')
+	for transform_size, rotation_bit_rate, translation_bit_rate in permutation_tries_no_scale:
+		print('\t{{ {}, {} }},\t\t// {} bits per transform'.format(rotation_bit_rate, translation_bit_rate, transform_size))
+	print('};')
+	print()
+	print('constexpr uint8_t k_local_bit_rate_permutations[{}][3] ='.format(len(permutation_tries)))
+	print('{')
+	for transform_size, rotation_bit_rate, translation_bit_rate, scale_bit_rate in permutation_tries:
+		print('\t{{ {}, {}, {} }},\t\t// {} bits per transform'.format(rotation_bit_rate, translation_bit_rate, scale_bit_rate, transform_size))
+	print('};')
