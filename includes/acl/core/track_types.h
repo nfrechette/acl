9efdbd9b47b7860a1174a18aa303c0a9b9d26acd
Nicholas Frechette <zeno490@gmail.com>
2020-07-05 23:24:46 -0400

    feat(compression): add default values

--- includes/acl/core/track_types.h
+++ includes/acl/core/track_types.h
@@ -164,14 +164,15 @@ namespace acl
 		// will be used instead of the track index. This allows custom reordering for things
 		// like LOD sorting or skeleton remapping. A value of 'k_invalid_track_index' will strip the track
 		// from the compressed data stream. Output indices must be unique and contiguous.
-		uint32_t output_index;
+		uint32_t output_index = k_invalid_track_index;
 
 		//////////////////////////////////////////////////////////////////////////
 		// The per component precision threshold to try and attain when optimizing the bit rate.
 		// If the error is below the precision threshold, we will remove bits until we reach it without
 		// exceeding it. If the error is above the precision threshold, we will add more bits until
 		// we lower it underneath.
-		float precision;
+		// Defaults to '0.00001'
+		float precision = 0.00001F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// Returns whether a scalar track description is valid or not.
@@ -200,22 +201,25 @@ namespace acl
 		// will be used instead of the track index. This allows custom reordering for things
 		// like LOD sorting or skeleton remapping. A value of 'k_invalid_track_index' will strip the track
 		// from the compressed data stream. Output indices must be unique and contiguous.
-		uint32_t output_index;
+		uint32_t output_index = k_invalid_track_index;
 
 		//////////////////////////////////////////////////////////////////////////
 		// The index of the parent transform track or `k_invalid_track_index` if it has no parent.
-		uint32_t parent_index;
+		uint32_t parent_index = k_invalid_track_index;
 
 		//////////////////////////////////////////////////////////////////////////
 		// The shell precision threshold to try and attain when optimizing the bit rate.
 		// If the error is below the precision threshold, we will remove bits until we reach it without
 		// exceeding it. If the error is above the precision threshold, we will add more bits until
 		// we lower it underneath.
-		float precision;
+		// Note that you will need to change this value if your units are not in centimeters.
+		// Defaults to '0.01' centimeters
+		float precision = 0.01F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// The error is measured on a rigidly deformed shell around every transform at the specified distance.
-		float shell_distance;
+		// Defaults to '3.0' centimeters
+		float shell_distance = 3.0F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// TODO: Use the precision and shell distance?
@@ -226,27 +230,27 @@ namespace acl
 		// was chosen. You will typically NEVER need to change this, the value has been
 		// selected to be as safe as possible and is independent of game engine units.
 		// Defaults to '0.00284714461' radians
-		float constant_rotation_threshold_angle;
+		float constant_rotation_threshold_angle = 0.00284714461F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// Threshold value to use when detecting if translation tracks are constant or default.
 		// Note that you will need to change this value if your units are not in centimeters.
 		// Defaults to '0.001' centimeters.
-		float constant_translation_threshold;
+		float constant_translation_threshold = 0.001F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// Threshold value to use when detecting if scale tracks are constant or default.
 		// There are no units for scale as such a value that was deemed safe was selected
 		// as a default.
 		// Defaults to '0.00001'
-		float constant_scale_threshold;
+		float constant_scale_threshold = 0.00001F;
 
 		//////////////////////////////////////////////////////////////////////////
 		// Returns whether a transform track description is valid or not.
 		// It is valid if:
 		//    - The precision is positive or zero and finite
 		//    - The shell distance is positive or zero and finite
-		//    - The constant toration threshold angle is positive or zero and finite
+		//    - The constant rotation threshold angle is positive or zero and finite
 		//    - The constant translation threshold is positive or zero and finite
 		//    - The constant scale threshold is positive or zero and finite
 		ErrorResult is_valid() const
