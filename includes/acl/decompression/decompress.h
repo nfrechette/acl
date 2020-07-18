79709b08922a71259a03074330b401e3b4448edb
Nicholas Frechette <zeno490@gmail.com>
2020-07-17 22:55:49 -0400

    refactor(decompression): move implementation details into separate header

--- includes/acl/decompression/decompress.h
+++ includes/acl/decompression/decompress.h
@@ -265,51 +265,8 @@ namespace acl
 	{
 		return allocate_type<decompression_context<decompression_settings_type>>(allocator);
 	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// decompression_context implementation
-
-	template<class decompression_settings_type>
-	inline decompression_context<decompression_settings_type>::decompression_context()
-		: m_context()
-	{
-		m_context.reset();
-	}
-
-	template<class decompression_settings_type>
-	inline bool decompression_context<decompression_settings_type>::initialize(const compressed_tracks& tracks)
-	{
-		if (!algorithm_version_type::is_version_supported(tracks.get_version()))
-			return false;
-
-		return algorithm_version_type::template initialize<decompression_settings_type>(m_context, tracks);
-	}
-
-	template<class decompression_settings_type>
-	inline bool decompression_context<decompression_settings_type>::is_dirty(const compressed_tracks& tracks) const
-	{
-		return algorithm_version_type::template is_dirty<decompression_settings_type>(m_context, tracks);
-	}
-
-	template<class decompression_settings_type>
-	inline void decompression_context<decompression_settings_type>::seek(float sample_time, sample_rounding_policy rounding_policy)
-	{
-		algorithm_version_type::template seek<decompression_settings_type>(m_context, sample_time, rounding_policy);
-	}
-
-	template<class decompression_settings_type>
-	template<class track_writer_type>
-	inline void decompression_context<decompression_settings_type>::decompress_tracks(track_writer_type& writer)
-	{
-		algorithm_version_type::template decompress_tracks<decompression_settings_type>(m_context, writer);
-	}
-
-	template<class decompression_settings_type>
-	template<class track_writer_type>
-	inline void decompression_context<decompression_settings_type>::decompress_track(uint32_t track_index, track_writer_type& writer)
-	{
-		algorithm_version_type::template decompress_track<decompression_settings_type>(m_context, track_index, writer);
-	}
 }
+
+#include "acl/decompression/impl/decompress.impl.h"
 
 ACL_IMPL_FILE_PRAGMA_POP
