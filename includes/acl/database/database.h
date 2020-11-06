be63ceea3c6fe4fa29815315d9579a99db196e35
Nicholas Frechette <zeno490@gmail.com>
2020-11-06 15:59:54 -0500

    style(database): fix wrong date

--- includes/acl/database/database.h
+++ includes/acl/database/database.h
@@ -3,7 +3,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 // The MIT License (MIT)
 //
-// Copyright (c) 2019 Nicholas Frechette & Animation Compression Library contributors
+// Copyright (c) 2020 Nicholas Frechette & Animation Compression Library contributors
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
