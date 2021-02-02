40e8a1338d56f3d287770168c14f719700b22f5b
Nicholas Frechette <zeno490@gmail.com>
2021-02-02 15:20:58 -0500

    fix(tests): use proper catch2 include path

--- tests/main_android/app/src/main/cpp/main.cpp
+++ tests/main_android/app/src/main/cpp/main.cpp
@@ -23,7 +23,7 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 #define CATCH_CONFIG_RUNNER
-#include <catch.hpp>
+#include <catch2/catch.hpp>
 
 #include <jni.h>
 
