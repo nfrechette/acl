b2ffc871d898e0e49428f308282404897c3ec340
Nicholas Frechette <zeno490@gmail.com>
2022-11-27 18:04:19 -0500

    feat(core): add array reverse iterator

--- includes/acl/core/iterator.h
+++ includes/acl/core/iterator.h
@@ -53,6 +53,22 @@ namespace acl
 			item_ptr_type	m_items;
 			size_t			m_num_items;
 		};
+
+		template <class item_type, bool is_const>
+		class array_reverse_iterator_impl
+		{
+		public:
+			using item_ptr_type = typename std::conditional<is_const, const item_type*, item_type*>::type;
+
+			constexpr array_reverse_iterator_impl(item_ptr_type items, size_t num_items) : m_items(items), m_num_items(num_items) {}
+
+			constexpr std::reverse_iterator<item_ptr_type> begin() const { return std::reverse_iterator<item_ptr_type>(m_items + m_num_items); }
+			constexpr std::reverse_iterator<item_ptr_type> end() const { return std::reverse_iterator<item_ptr_type>(m_items); }
+
+		private:
+			item_ptr_type	m_items;
+			size_t			m_num_items;
+		};
 	}
 
 	//ACL_DEPRECATED("Renamed to array_iterator, to be removed in v3.0")
@@ -91,6 +107,36 @@ namespace acl
 	const_array_iterator<item_type> make_iterator(const item_type* items, size_t num_items)
 	{
 		return const_array_iterator<item_type>(items, num_items);
+	}
+
+	template <class item_type>
+	using array_reverse_iterator = acl_impl::array_reverse_iterator_impl<item_type, false>;
+
+	template <class item_type>
+	using const_array_reverse_iterator = acl_impl::array_reverse_iterator_impl<item_type, true>;
+
+	template <class item_type, size_t num_items>
+	array_reverse_iterator<item_type> make_reverse_iterator(item_type(&items)[num_items])
+	{
+		return array_reverse_iterator<item_type>(items, num_items);
+	}
+
+	template <class item_type, size_t num_items>
+	const_array_reverse_iterator<item_type> make_reverse_iterator(item_type const (&items)[num_items])
+	{
+		return const_array_reverse_iterator<item_type>(items, num_items);
+	}
+
+	template <class item_type>
+	array_reverse_iterator<item_type> make_reverse_iterator(item_type* items, size_t num_items)
+	{
+		return array_reverse_iterator<item_type>(items, num_items);
+	}
+
+	template <class item_type>
+	const_array_reverse_iterator<item_type> make_reverse_iterator(const item_type* items, size_t num_items)
+	{
+		return const_array_reverse_iterator<item_type>(items, num_items);
 	}
 
 	ACL_IMPL_VERSION_NAMESPACE_END
