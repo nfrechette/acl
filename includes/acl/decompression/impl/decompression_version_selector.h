21c66feaea37bd699ca7a8ac1522449b829d781e
Nicholas Frechette <zeno490@gmail.com>
2022-12-04 00:02:38 -0500

    refactor: deprecate track description constant thresholds
    Fixes #441

--- includes/acl/decompression/impl/decompression_version_selector.h
+++ includes/acl/decompression/impl/decompression_version_selector.h
@@ -106,6 +106,30 @@ namespace acl
 			RTM_FORCE_INLINE static void decompress_track(context_type& context, uint32_t track_index, track_writer_type& writer) { acl_impl::decompress_track_v0<decompression_settings_type>(context, track_index, writer); }
 		};
 
+		template<>
+		struct decompression_version_selector<compressed_tracks_version16::v02_01_99_1>
+		{
+			static constexpr bool is_version_supported(compressed_tracks_version16 version) { return version == compressed_tracks_version16::v02_01_99_1; }
+
+			template<class decompression_settings_type, class context_type, class database_settings_type>
+			RTM_FORCE_INLINE static bool initialize(context_type& context, const compressed_tracks& tracks, const database_context<database_settings_type>* database) { return acl_impl::initialize_v0<decompression_settings_type>(context, tracks, database); }
+
+			template<class context_type>
+			RTM_FORCE_INLINE static bool is_dirty(const context_type& context, const compressed_tracks& tracks) { return acl_impl::is_dirty_v0(context, tracks); }
+
+			template<class decompression_settings_type, class context_type>
+			RTM_FORCE_INLINE static void set_looping_policy(context_type& context, sample_looping_policy policy) { acl_impl::set_looping_policy_v0<decompression_settings_type>(context, policy); }
+
+			template<class decompression_settings_type, class context_type>
+			RTM_FORCE_INLINE static void seek(context_type& context, float sample_time, sample_rounding_policy rounding_policy) { acl_impl::seek_v0<decompression_settings_type>(context, sample_time, rounding_policy); }
+
+			template<class decompression_settings_type, class track_writer_type, class context_type>
+			RTM_FORCE_INLINE static void decompress_tracks(context_type& context, track_writer_type& writer) { acl_impl::decompress_tracks_v0<decompression_settings_type>(context, writer); }
+
+			template<class decompression_settings_type, class track_writer_type, class context_type>
+			RTM_FORCE_INLINE static void decompress_track(context_type& context, uint32_t track_index, track_writer_type& writer) { acl_impl::decompress_track_v0<decompression_settings_type>(context, track_index, writer); }
+		};
+
 		//////////////////////////////////////////////////////////////////////////
 		// Not optimized for any particular version.
 		//////////////////////////////////////////////////////////////////////////
@@ -126,6 +150,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					return acl_impl::initialize_v0<decompression_settings_type>(context, tracks, database);
 				default:
 					ACL_ASSERT(false, "Unsupported version");
@@ -141,6 +166,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					return acl_impl::is_dirty_v0(context, tracks);
 				default:
 					ACL_ASSERT(false, "Unsupported version");
@@ -156,6 +182,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					acl_impl::set_looping_policy_v0<decompression_settings_type>(context, policy);
 					break;
 				default:
@@ -172,6 +199,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					acl_impl::seek_v0<decompression_settings_type>(context, sample_time, rounding_policy);
 					break;
 				default:
@@ -188,6 +216,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					acl_impl::decompress_tracks_v0<decompression_settings_type>(context, writer);
 					break;
 				default:
@@ -204,6 +233,7 @@ namespace acl
 				{
 				case compressed_tracks_version16::v02_00_00:
 				case compressed_tracks_version16::v02_01_99:
+				case compressed_tracks_version16::v02_01_99_1:
 					acl_impl::decompress_track_v0<decompression_settings_type>(context, track_index, writer);
 					break;
 				default:
