9b5a5206aacd0dd9f9cfc729ea7422f1259cfc09
Nicholas Frechette <zeno490@gmail.com>
2023-04-22 21:20:16 -0400

    chore: add xcode14 support for CI

--- README.md
+++ README.md
@@ -50,9 +50,9 @@ Much thought was put into designing the library for it to be as flexible and pow
 *  Linux (gcc 5 to 13) x86 and x64
 *  Linux (clang 4 to 15) x86 and x64
 *  OS X (Xcode 10.3) x86 and x64
-*  OS X (Xcode 11.7, 12.5, 13.2) x64 and ARM64
+*  OS X (Xcode 11.7, 12.5, 13.2, 14.2) x64 and ARM64
 *  Android (NDK 21) ARMv7-A and ARM64
-*  iOS (Xcode 10.3, 11.7, 12.5, 13.2) ARM64
+*  iOS (Xcode 10.3, 11.7, 12.5, 13.2, 14.2) ARM64
 *  Emscripten (1.39.11) WASM
 
 The above supported platform list is only what is tested every release but if it compiles, it should run just fine.
--- tools/setup_osx_compiler.sh
+++ tools/setup_osx_compiler.sh
@@ -3,6 +3,14 @@
 # Extract our command line arguments
 COMPILER=$1
 
+# See Github hosted runners:
+# macos-12: https://github.com/actions/runner-images/blob/main/images/macos/macos-12-Readme.md
+#   xcode 13.1, 12.2.1, 13.3.1, 13.4.1, 14.1, 14.2
+# maxos-11: https://github.com/actions/runner-images/blob/main/images/macos/macos-11-Readme.md
+#   xcode 11.7, 12.4, 12.5.1, 13.0, 13.1, 13.2.1
+# maxos-10.15: https://github.com/actions/runner-images/blob/main/images/macos/macos-10.15-Readme.md
+#   xcode 10.3, 11.2.1, 11.3.1, 11.4.1, 11.5, 11.6, 11.7, 12, 12.1, 12.1.1, 12.2, 12.3, 12.4
+
 # Convert our compiler string into our XCode path
 # Paths must match Github Action virtual images
 if [[ $COMPILER == xcode10 ]]; then
@@ -13,6 +21,8 @@ elif [[ $COMPILER == xcode12 ]]; then
     XCODE_PATH="/Applications/Xcode_12.5.1.app"
 elif [[ $COMPILER == xcode13 ]]; then
     XCODE_PATH="/Applications/Xcode_13.2.1.app"
+elif [[ $COMPILER == xcode14 ]]; then
+    XCODE_PATH="/Applications/Xcode_14.2.app"
 fi
 
 # Select our XCode version
