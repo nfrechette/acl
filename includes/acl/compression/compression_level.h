d7252410cd2646e0a96d3e79b56ceddfc6a919a3
Nicholas Frechette <zeno490@gmail.com>
2023-05-10 22:19:11 -0400

    refactor(compression): split compression level header file

--- includes/acl/compression/compression_level.h
+++ includes/acl/compression/compression_level.h
@@ -57,76 +57,15 @@ namespace acl
 	////////////////////////////////////////////////////////////////////////////////
 	// Returns a string representing the compression level.
 	// TODO: constexpr
-	inline const char* get_compression_level_name(compression_level8 level)
-	{
-		switch (level)
-		{
-		case compression_level8::lowest:	return "lowest";
-		case compression_level8::low:		return "low";
-		case compression_level8::medium:	return "medium";
-		case compression_level8::high:		return "high";
-		case compression_level8::highest:	return "highest";
-		default:							return "<Invalid>";
-		}
-	}
+	inline const char* get_compression_level_name(compression_level8 level);
 
 	//////////////////////////////////////////////////////////////////////////
 	// Returns the compression level from its string representation.
-	inline bool get_compression_level(const char* level_name, compression_level8& out_level)
-	{
-		ACL_ASSERT(level_name != nullptr, "Level name cannot be null");
-		if (level_name == nullptr)
-			return false;
-
-		const char* level_lowest = "Lowest";	// ACL_DEPRECATED Legacy name, keep for backwards compatibility, remove in 3.0
-		const char* level_lowest_new = "lowest";
-		if (std::strncmp(level_name, level_lowest, std::strlen(level_lowest)) == 0
-			|| std::strncmp(level_name, level_lowest_new, std::strlen(level_lowest_new)) == 0)
-		{
-			out_level = compression_level8::lowest;
-			return true;
-		}
-
-		const char* level_low = "Low";	// ACL_DEPRECATED Legacy name, keep for backwards compatibility, remove in 3.0
-		const char* level_low_new = "low";
-		if (std::strncmp(level_name, level_low, std::strlen(level_low)) == 0
-			|| std::strncmp(level_name, level_low_new, std::strlen(level_low_new)) == 0)
-		{
-			out_level = compression_level8::low;
-			return true;
-		}
-
-		const char* level_medium = "Medium";	// ACL_DEPRECATED Legacy name, keep for backwards compatibility, remove in 3.0
-		const char* level_medium_new = "medium";
-		if (std::strncmp(level_name, level_medium, std::strlen(level_medium)) == 0
-			|| std::strncmp(level_name, level_medium_new, std::strlen(level_medium_new)) == 0)
-		{
-			out_level = compression_level8::medium;
-			return true;
-		}
-
-		const char* level_highest = "Highest";	// ACL_DEPRECATED Legacy name, keep for backwards compatibility, remove in 3.0
-		const char* level_highest_new = "highest";
-		if (std::strncmp(level_name, level_highest, std::strlen(level_highest)) == 0
-			|| std::strncmp(level_name, level_highest_new, std::strlen(level_highest_new)) == 0)
-		{
-			out_level = compression_level8::highest;
-			return true;
-		}
-
-		const char* level_high = "High";	// ACL_DEPRECATED Legacy name, keep for backwards compatibility, remove in 3.0
-		const char* level_high_new = "high";
-		if (std::strncmp(level_name, level_high, std::strlen(level_high)) == 0
-			|| std::strncmp(level_name, level_high_new, std::strlen(level_high_new)) == 0)
-		{
-			out_level = compression_level8::high;
-			return true;
-		}
-
-		return false;
-	}
+	inline bool get_compression_level(const char* level_name, compression_level8& out_level);
 
 	ACL_IMPL_VERSION_NAMESPACE_END
 }
+
+#include "acl/compression/impl/compression_level.impl.h"
 
 ACL_IMPL_FILE_PRAGMA_POP
