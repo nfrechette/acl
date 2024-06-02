5ed21fcc7e85588cc22bb5fbe6099af0756cb40f
Nicholas Frechette <zeno490@gmail.com>
2024-06-01 21:05:58 -0400

    fix(compression): only reset when verbose

--- includes/acl/compression/impl/quantize.transform.h
+++ includes/acl/compression/impl/quantize.transform.h
@@ -2571,10 +2571,10 @@ namespace acl
 					{
 #if ACL_IMPL_DEBUG_VARIABLE_QUANTIZATION >= ACL_IMPL_DEBUG_LEVEL_VERBOSE_INFO
 						printf("%8u: [%3u | %3u | %3u] best with %2u bits @ %.4f\n", transform_index, best_transform_bit_rates.rotation, best_transform_bit_rates.translation, best_transform_bit_rates.scale, prev_transform_size, best_transform_error);
-#endif
 
 						// Reset
 						best_transform_error = 1.0E10F;
+#endif
 
 #if ACL_IMPL_DEBUG_VARIABLE_QUANTIZATION >= ACL_IMPL_DEBUG_LEVEL_BASIC_INFO && 0
 						best_permutation_group_optimization_room = FLT_MAX;
