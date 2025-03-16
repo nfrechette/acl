903f2c3a344ef3cab82df95412a9598cbd3cd813
Nicholas Frechette <zeno490@gmail.com>
2025-03-16 17:13:57 -0400

    style(core): fix indentation

--- includes/acl/core/bit_manip_utils.h
+++ includes/acl/core/bit_manip_utils.h
@@ -191,11 +191,11 @@ namespace acl
 	{
 #if defined(ACL_BMI_INTRINSICS)
 		// Use BMI
-#if defined(RTM_COMPILER_GCC) && !defined(_andn_u32)
+	#if defined(RTM_COMPILER_GCC) && !defined(_andn_u32)
 		return __andn_u32(not_value, and_value);	// GCC doesn't define the right intrinsic symbol
-#else
+	#else
 		return _andn_u32(not_value, and_value);
-#endif
+	#endif
 #else
 		return ~not_value & and_value;
 #endif
