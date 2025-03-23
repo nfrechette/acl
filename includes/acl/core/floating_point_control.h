70caf2660871809655cd0b976fbc4c584411c2c3
Nicholas Frechette <zeno490@gmail.com>
2025-03-22 20:25:00 -0400

    feat(core): add ability to enable/disable flush denormals to zero

--- includes/acl/core/floating_point_control.h
+++ includes/acl/core/floating_point_control.h
@@ -39,7 +39,10 @@ namespace acl
 	struct fp_environment
 	{
 #if defined(RTM_SSE2_INTRINSICS)
+		// TODO: It would be nice to clean this up and just cache the MXCSR register, see _mm_getcsr
 		unsigned int exception_mask = 0;
+		unsigned int flush_zero_mode = 0;
+		unsigned int denormals_zero_mode = 0;
 #elif defined(RTM_NEON_INTRINSICS)
 		// TODO: Implement on ARM. API to do this is not consistent across Android, Windows ARM, and iOS
 		// and on top of it, most ARM CPUs out there do not raise the SIGFPE trap so they are silent
@@ -95,6 +98,53 @@ namespace acl
 #endif
 	}
 
+	// Enables flushing floating point denormals to zero.
+	inline void enable_flush_to_zero_fp_denormals(fp_environment& out_old_env)
+	{
+#if defined(RTM_SSE2_INTRINSICS)
+		// We only care about SSE and not x87
+		// Cache the flush to zero mask we had so we can restore it later
+		out_old_env.flush_zero_mode = _MM_GET_FLUSH_ZERO_MODE();
+		out_old_env.denormals_zero_mode = _MM_GET_DENORMALS_ZERO_MODE();
+
+		// Enable flushing
+		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
+		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_ON);
+#else
+		(void)out_old_env;
+#endif
+	}
+
+	// Disables flushing floating point denormals to zero.
+	inline void disable_flush_to_zero_fp_denormals(fp_environment& out_old_env)
+	{
+#if defined(RTM_SSE2_INTRINSICS)
+		// We only care about SSE and not x87
+		// Cache the flush to zero mask we had so we can restore it later
+		out_old_env.flush_zero_mode = _MM_GET_FLUSH_ZERO_MODE();
+		out_old_env.denormals_zero_mode = _MM_GET_DENORMALS_ZERO_MODE();
+
+		// Disable flushing
+		_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_OFF);
+		_MM_SET_DENORMALS_ZERO_MODE(_MM_DENORMALS_ZERO_OFF);
+#else
+		(void)out_old_env;
+#endif
+	}
+
+	// Restores a previously set floating point flush-to-zero state.
+	inline void restore_flush_to_zero_fp_denormals(const fp_environment& env)
+	{
+#if defined(RTM_SSE2_INTRINSICS)
+		// We only care about SSE and not x87
+		// Restore our old value
+		_MM_SET_FLUSH_ZERO_MODE(env.flush_zero_mode);
+		_MM_SET_DENORMALS_ZERO_MODE(env.denormals_zero_mode);
+#else
+		(void)env;
+#endif
+	}
+
 	// Enables floating point exceptions in the parent scope for invalid operations, division by zero, and overflow.
 	class scope_enable_fp_exceptions
 	{
@@ -135,6 +185,50 @@ namespace acl
 		// Prevent copy or move
 		scope_disable_fp_exceptions(const scope_disable_fp_exceptions&) = delete;
 		scope_disable_fp_exceptions& operator=(const scope_disable_fp_exceptions&) = delete;
+
+		fp_environment env;
+	};
+
+	// Enables flushing floating point denormals to zero.
+	class scope_enable_flush_to_zero_fp_denormals
+	{
+	public:
+		scope_enable_flush_to_zero_fp_denormals()
+		{
+			enable_flush_to_zero_fp_denormals(env);
+		}
+
+		~scope_enable_flush_to_zero_fp_denormals()
+		{
+			restore_flush_to_zero_fp_denormals(env);
+		}
+
+	private:
+		// Prevent copy or move
+		scope_enable_flush_to_zero_fp_denormals(const scope_enable_flush_to_zero_fp_denormals&) = delete;
+		scope_enable_flush_to_zero_fp_denormals& operator=(const scope_enable_flush_to_zero_fp_denormals&) = delete;
+
+		fp_environment env;
+	};
+
+	// Disables flushing floating point denormals to zero.
+	class scope_disable_flush_to_zero_fp_denormals
+	{
+	public:
+		scope_disable_flush_to_zero_fp_denormals()
+		{
+			disable_flush_to_zero_fp_denormals(env);
+		}
+
+		~scope_disable_flush_to_zero_fp_denormals()
+		{
+			restore_flush_to_zero_fp_denormals(env);
+		}
+
+	private:
+		// Prevent copy or move
+		scope_disable_flush_to_zero_fp_denormals(const scope_disable_flush_to_zero_fp_denormals&) = delete;
+		scope_disable_flush_to_zero_fp_denormals& operator=(const scope_disable_flush_to_zero_fp_denormals&) = delete;
 
 		fp_environment env;
 	};
