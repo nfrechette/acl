ed45127e809ecd26b52c6871e3be025b6136d853
Nicholas Frechette <zeno490@gmail.com>
2025-04-09 21:54:23 -0400

    feat(decompression): add default sub-track variants

--- includes/acl/decompression/impl/steps/scale_default.h
+++ includes/acl/decompression/impl/steps/scale_default.h
@@ -32,6 +32,11 @@
 
 #include <cstdint>
 
+// Controls which variant to use for this decompression step
+// 0: ACL 2.1 (baseline + minor tweaks)
+// 1: Simple bit scan
+#define ACL_IMPL_STEP_CURRENT_VARIANT 0
+
 ACL_IMPL_FILE_PRAGMA_PUSH
 
 namespace acl
@@ -40,6 +45,7 @@ namespace acl
 
 	namespace acl_impl
 	{
+#if 0
 		template<class track_writer_type>
 		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_unpack_default_scales(
 			step_context_t& step_context, rtm::vector4f_arg0 default_scale_,
@@ -206,6 +212,194 @@ namespace acl
 		done:
 			step_context.prefetch_queue_ptr = prefetch_queue_ptr;
 		}
+#elif ACL_IMPL_STEP_CURRENT_VARIANT == 0 // ACL 2.1 (baseline + minor tweaks)
+		template<class track_writer_type>
+		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_unpack_default_scales(
+			step_context_t& step_context, rtm::vector4f_arg0 default_scale_,
+			track_writer_type& writer)
+		{
+			if (track_writer_type::skip_all_scales())
+				return;
+
+			constexpr default_sub_track_mode default_mode = track_writer_type::get_default_scale_mode();
+			if (default_mode == default_sub_track_mode::skipped)
+				return;	// Nothing to write
+
+			const packed_sub_track_types* scale_sub_track_types = step_context.scale_sub_track_types;
+			const uint32_t last_entry_index = step_context.last_entry_index;
+			const uint32_t padding_mask = step_context.padding_mask;
+
+			const packed_sub_track_types* scale_sub_track_types_last = scale_sub_track_types + last_entry_index;
+
+			// Grab our constant default scale if we have one, otherwise init with some value
+			rtm::vector4f default_scale;
+			if (default_mode == default_sub_track_mode::constant)
+				default_scale = writer.get_constant_default_scale();
+			else if (default_mode == default_sub_track_mode::legacy)
+				default_scale = default_scale_;
+			else
+				default_scale = rtm::vector_zero();
+
+			uint32_t track_index = 0;
+
+			while (scale_sub_track_types <= scale_sub_track_types_last)
+			{
+				uint32_t packed_entry = scale_sub_track_types->types;
+
+				// Mask out everything but default sub-tracks, this way we can early out when we iterate
+				// Each sub-track is either 0 (default), 1 (constant), or 2 (animated)
+				// By flipping the bits with logical NOT, 0 becomes 3, 1 becomes 2, and 2 becomes 1
+				// We then subtract 1 from every group so 3 becomes 2, 2 becomes 1, and 1 becomes 0
+				// Finally, we mask out everything but the second bit for each sub-track
+				// After this, our original default tracks are equal to 2, our constant tracks are equal to 1, and our animated tracks are equal to 0
+				// Testing for default tracks can be done by testing the second bit of each group (same as animated track testing)
+				packed_entry = ~packed_entry - 0x55555555;
+
+				// Because our last entry might have padding with 0 (default), we have to strip any padding we might have
+				const uint32_t entry_padding_mask = scale_sub_track_types == scale_sub_track_types_last ? padding_mask : 0xAAAAAAAA;
+				packed_entry &= entry_padding_mask;
+
+				uint32_t curr_entry_track_index = track_index;
+
+				// We might early out below, always skip 16 tracks
+				track_index += 16;
+				scale_sub_track_types++;
+
+				// Process 4 sub-tracks at a time
+				while (packed_entry != 0)
+				{
+					const uint32_t packed_group = packed_entry;
+					const uint32_t curr_group_track_index = curr_entry_track_index;
+
+					// Move to the next group
+					packed_entry <<= 8;
+					curr_entry_track_index += 4;
+
+					if ((packed_group & 0xAA000000) == 0)
+						continue;	// This group contains no default sub-tracks, skip it
+
+					if ((packed_group & 0x80000000) != 0)
+					{
+						const uint32_t track_index0 = curr_group_track_index + 0;
+
+						if (!track_writer_type::skip_all_scales() && !writer.skip_track_scale(track_index0))
+						{
+							if (default_mode == default_sub_track_mode::variable)
+								writer.write_scale(track_index0, writer.get_variable_default_scale(track_index0));
+							else
+								writer.write_scale(track_index0, default_scale);
+						}
+					}
+
+					if ((packed_group & 0x20000000) != 0)
+					{
+						const uint32_t track_index1 = curr_group_track_index + 1;
+
+						if (!track_writer_type::skip_all_scales() && !writer.skip_track_scale(track_index1))
+						{
+							if (default_mode == default_sub_track_mode::variable)
+								writer.write_scale(track_index1, writer.get_variable_default_scale(track_index1));
+							else
+								writer.write_scale(track_index1, default_scale);
+						}
+					}
+
+					if ((packed_group & 0x08000000) != 0)
+					{
+						const uint32_t track_index2 = curr_group_track_index + 2;
+
+						if (!track_writer_type::skip_all_scales() && !writer.skip_track_scale(track_index2))
+						{
+							if (default_mode == default_sub_track_mode::variable)
+								writer.write_scale(track_index2, writer.get_variable_default_scale(track_index2));
+							else
+								writer.write_scale(track_index2, default_scale);
+						}
+					}
+
+					if ((packed_group & 0x02000000) != 0)
+					{
+						const uint32_t track_index3 = curr_group_track_index + 3;
+
+						if (!track_writer_type::skip_all_scales() && !writer.skip_track_scale(track_index3))
+						{
+							if (default_mode == default_sub_track_mode::variable)
+								writer.write_scale(track_index3, writer.get_variable_default_scale(track_index3));
+							else
+								writer.write_scale(track_index3, default_scale);
+						}
+					}
+				}
+			}
+		}
+#elif ACL_IMPL_STEP_CURRENT_VARIANT == 1 // Simple bit scan
+		template<class track_writer_type>
+		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_unpack_default_scales(
+			step_context_t& step_context, rtm::vector4f_arg0 default_scale_,
+			track_writer_type& writer)
+		{
+			if (track_writer_type::skip_all_scales())
+				return;
+
+			constexpr default_sub_track_mode default_mode = track_writer_type::get_default_scale_mode();
+			if (default_mode == default_sub_track_mode::skipped)
+				return;	// Nothing to write
+
+			const packed_sub_track_types* scale_sub_track_types = step_context.scale_sub_track_types;
+			const uint32_t last_entry_index = step_context.last_entry_index;
+			const uint32_t padding_mask = step_context.padding_mask;
+
+			// Grab our constant default scale if we have one, otherwise init with some value
+			rtm::vector4f default_scale;
+			if (default_mode == default_sub_track_mode::constant)
+				default_scale = writer.get_constant_default_scale();
+			else if (default_mode == default_sub_track_mode::legacy)
+				default_scale = default_scale_;
+			else
+				default_scale = rtm::vector_zero();
+
+			for (uint32_t entry_index = 0; entry_index <= last_entry_index; ++entry_index)
+			{
+				uint32_t packed_entry = scale_sub_track_types[entry_index].types;
+
+				// Mask out everything but default sub-tracks, this way we can early out when we iterate
+				// Each sub-track is either 0 (default), 1 (constant), or 2 (animated)
+				// By flipping the bits with logical NOT, 0 becomes 3, 1 becomes 2, and 2 becomes 1
+				// We then subtract 1 from every group so 3 becomes 2, 2 becomes 1, and 1 becomes 0
+				// Finally, we mask out everything but the second bit for each sub-track
+				// After this, our original default tracks are equal to 2, our constant tracks are equal to 1, and our animated tracks are equal to 0
+				// Testing for default tracks can be done by testing the second bit of each group (same as animated track testing)
+				packed_entry = ~packed_entry - 0x55555555;
+
+				// Because our last entry might have padding with 0 (default), we have to strip any padding we might have
+				const uint32_t entry_padding_mask = (entry_index == last_entry_index) ? padding_mask : 0xAAAAAAAA;
+				packed_entry &= entry_padding_mask;
+
+				// We have 2 bits per sub-track
+				const uint32_t curr_entry_track_index = entry_index * 16;
+
+				while (packed_entry != 0)
+				{
+					const uint32_t set_bit_index = count_leading_zeros(packed_entry);
+					const uint32_t highest_set_bit = 1 << (31 - set_bit_index);
+
+					// Mask out the bit we just consumed
+					packed_entry ^= highest_set_bit;
+
+					// We have 2 bits per sub-track
+					const uint32_t track_index = curr_entry_track_index + (set_bit_index / 2);
+
+					if (!writer.skip_track_scale(track_index))
+					{
+						if (default_mode == default_sub_track_mode::variable)
+							writer.write_scale(track_index, writer.get_variable_default_scale(track_index));
+						else
+							writer.write_scale(track_index, default_scale);
+					}
+				}
+			}
+		}
+#endif
 
 		template<class track_writer_type>
 		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_set_default_scales(
@@ -299,5 +493,7 @@ namespace acl
 
 	ACL_IMPL_VERSION_NAMESPACE_END
 }
+
+#undef ACL_IMPL_STEP_CURRENT_VARIANT
 
 ACL_IMPL_FILE_PRAGMA_POP
--- includes/acl/decompression/impl/steps/translation_default.h
+++ includes/acl/decompression/impl/steps/translation_default.h
@@ -32,6 +32,11 @@
 
 #include <cstdint>
 
+// Controls which variant to use for this decompression step
+// 0: ACL 2.1 (baseline + minor tweaks)
+// 1: Simple bit scan
+#define ACL_IMPL_STEP_CURRENT_VARIANT 0
+
 ACL_IMPL_FILE_PRAGMA_PUSH
 
 namespace acl
@@ -202,7 +207,7 @@ namespace acl
 		done:
 			step_context.prefetch_queue_ptr = prefetch_queue_ptr;
 		}
-#else
+#elif ACL_IMPL_STEP_CURRENT_VARIANT == 0 // ACL 2.1 (baseline + minor tweaks)
 		template<class track_writer_type>
 		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_unpack_default_translations(
 			step_context_t& step_context,
@@ -225,6 +230,8 @@ namespace acl
 			const uint32_t padding_mask = step_context.padding_mask;
 			//const void** prefetch_queue_ptr = step_context.prefetch_queue_ptr;
 
+			const packed_sub_track_types* translation_sub_track_types_last = translation_sub_track_types + last_entry_index;
+
 			// Cache the next prefetch ptr to avoid reloading it each loop iteration
 			// This way, the branch can easily be predicted because once we are done
 			// prefetching every entry, the ptr will remain forever null and this
@@ -234,9 +241,11 @@ namespace acl
 			// Grab our constant default translation if we have one, otherwise init with some value
 			const rtm::vector4f default_translation = default_mode == default_sub_track_mode::constant ? writer.get_constant_default_translation() : rtm::vector_zero();
 
-			for (uint32_t entry_index = 0, track_index = 0; entry_index <= last_entry_index; ++entry_index)
+			uint32_t track_index = 0;
+
+			while (translation_sub_track_types <= translation_sub_track_types_last)
 			{
-				uint32_t packed_entry = translation_sub_track_types[entry_index].types;
+				uint32_t packed_entry = translation_sub_track_types->types;
 
 				// Mask out everything but default sub-tracks, this way we can early out when we iterate
 				// Each sub-track is either 0 (default), 1 (constant), or 2 (animated)
@@ -248,13 +257,14 @@ namespace acl
 				packed_entry = ~packed_entry - 0x55555555;
 
 				// Because our last entry might have padding with 0 (default), we have to strip any padding we might have
-				const uint32_t entry_padding_mask = (entry_index == last_entry_index) ? padding_mask : 0xAAAAAAAA;
+				const uint32_t entry_padding_mask = translation_sub_track_types == translation_sub_track_types_last ? padding_mask : 0xAAAAAAAA;
 				packed_entry &= entry_padding_mask;
 
 				uint32_t curr_entry_track_index = track_index;
 
 				// We might early out below, always skip 16 tracks
 				track_index += 16;
+				translation_sub_track_types++;
 
 				// Process 4 sub-tracks at a time
 				while (packed_entry != 0)
@@ -323,10 +333,85 @@ namespace acl
 				}
 			}
 		}
+#elif ACL_IMPL_STEP_CURRENT_VARIANT == 1 // Simple bit scan
+		template<class track_writer_type>
+		ACL_IMPL_DEBUG_FORCE_INLINE RTM_DISABLE_SECURITY_COOKIE_CHECK void RTM_SIMD_CALL step_unpack_default_translations(
+			step_context_t& step_context,
+			track_writer_type& writer)
+		{
+			// On Apply M1:
+			//   - Warm CPU Cache: ???? IPC (default impl)
+			//   - Cold CPU cache: ???? IPC (default impl)
+
+			if (track_writer_type::skip_all_translations())
+				return;
+
+			constexpr default_sub_track_mode default_mode = track_writer_type::get_default_translation_mode();
+			static_assert(default_mode != default_sub_track_mode::legacy, "Not supported for translations");
+			if (default_mode == default_sub_track_mode::skipped)
+				return;
+
+			const packed_sub_track_types* translation_sub_track_types = step_context.translation_sub_track_types;
+			const uint32_t last_entry_index = step_context.last_entry_index;
+			const uint32_t padding_mask = step_context.padding_mask;
+			//const void** prefetch_queue_ptr = step_context.prefetch_queue_ptr;
+
+			// Cache the next prefetch ptr to avoid reloading it each loop iteration
+			// This way, the branch can easily be predicted because once we are done
+			// prefetching every entry, the ptr will remain forever null and this
+			// the branch is always constant: not zero for some time, then forever zero
+			//const void* next_prefetch_ptr = *prefetch_queue_ptr;
+
+			// Grab our constant default translation if we have one, otherwise init with some value
+			const rtm::vector4f default_translation = default_mode == default_sub_track_mode::constant ? writer.get_constant_default_translation() : rtm::vector_zero();
+
+			for (uint32_t entry_index = 0; entry_index <= last_entry_index; ++entry_index)
+			{
+				uint32_t packed_entry = translation_sub_track_types[entry_index].types;
+
+				// Mask out everything but default sub-tracks, this way we can early out when we iterate
+				// Each sub-track is either 0 (default), 1 (constant), or 2 (animated)
+				// By flipping the bits with logical NOT, 0 becomes 3, 1 becomes 2, and 2 becomes 1
+				// We then subtract 1 from every group so 3 becomes 2, 2 becomes 1, and 1 becomes 0
+				// Finally, we mask out everything but the second bit for each sub-track
+				// After this, our original default tracks are equal to 2, our constant tracks are equal to 1, and our animated tracks are equal to 0
+				// Testing for default tracks can be done by testing the second bit of each group (same as animated track testing)
+				packed_entry = ~packed_entry - 0x55555555;
+
+				// Because our last entry might have padding with 0 (default), we have to strip any padding we might have
+				const uint32_t entry_padding_mask = (entry_index == last_entry_index) ? padding_mask : 0xAAAAAAAA;
+				packed_entry &= entry_padding_mask;
+
+				// We have 2 bits per sub-track
+				const uint32_t curr_entry_track_index = entry_index * 16;
+
+				while (packed_entry != 0)
+				{
+					const uint32_t set_bit_index = count_leading_zeros(packed_entry);
+					const uint32_t highest_set_bit = 1 << (31 - set_bit_index);
+
+					// Mask out the bit we just consumed
+					packed_entry ^= highest_set_bit;
+
+					// We have 2 bits per sub-track
+					const uint32_t track_index = curr_entry_track_index + (set_bit_index / 2);
+
+					if (!writer.skip_track_translation(track_index))
+					{
+						if (default_mode == default_sub_track_mode::variable)
+							writer.write_translation(track_index, writer.get_variable_default_translation(track_index));
+						else
+							writer.write_translation(track_index, default_translation);
+					}
+				}
+			}
+		}
 #endif
 	}
 
 	ACL_IMPL_VERSION_NAMESPACE_END
 }
+
+#undef ACL_IMPL_STEP_CURRENT_VARIANT
 
 ACL_IMPL_FILE_PRAGMA_POP
