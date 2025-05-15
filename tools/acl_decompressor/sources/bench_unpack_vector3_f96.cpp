4fdc1e695fb7c246126d2dee37e0cb85de118170
Nicholas Frechette <zeno490@gmail.com>
2025-05-14 22:48:54 -0400

    feat(bench): add vec3 unpack f96 SSE variants

--- tools/acl_decompressor/sources/bench_unpack_vector3_f96.cpp
+++ tools/acl_decompressor/sources/bench_unpack_vector3_f96.cpp
@@ -192,7 +192,83 @@ rtm::vector4f RTM_SIMD_CALL unpack_vector3_f96_sse2_v0(
 
 	return _mm_castsi128_ps(_mm_set_epi32(static_cast<int32_t>(x32), static_cast<int32_t>(z32), static_cast<int32_t>(y32), static_cast<int32_t>(x32)));
 }
+
+RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_NOINLINE
+rtm::vector4f RTM_SIMD_CALL unpack_vector3_f96_sse2_v1(
+	const uint8_t* vector_data,
+	uint32_t bit_offset)
+{
+	// Same principle as NEON v1
+	const uint32_t byte_offset = bit_offset / 8;
+	const uint32_t shift_offset = bit_offset % 8;
+
+	// Load 16 bytes
+	// [0,1,2,3,4,5,6,7], [8,9,10,11,12,13,14,15]
+	const __m128i raw_bytes = _mm_loadu_si128((const __m128i*)(vector_data + byte_offset));
+
+	// [_,0,_,2,_,4,_,6], [_,8,_,10,_,12,_,14]
+	const __m128i even_shifted = _mm_srli_epi16(raw_bytes, 8);
+	// [1,_,3,_,5,_,7,_], [9,_,11,_,13,_,15,_]
+	const __m128i odd_shifted = _mm_slli_epi16(raw_bytes, 8);
+	// [1,0,3,2,5,4,7,6], [9,8,11,10,13,12,15,14]
+	__m128i rev_raw_bytes = _mm_or_si128(even_shifted, odd_shifted);
+	// [7,6,5,4,3,2,1,0], [9,8,11,10,13,12,15,14]
+	rev_raw_bytes = _mm_shufflelo_epi16(rev_raw_bytes, 0x1B);
+	// [7,6,5,4,3,2,1,0], [15,14,13,12,11,10,9,8]
+	rev_raw_bytes = _mm_shufflehi_epi16(rev_raw_bytes, 0x1B);
+
+	// Select each component and combine our pairs
+	// [7,6,5,4,3,2,1,0], [11,10,9,8,7,6,5,4]
+	__m128i xy = _mm_shuffle_epi32(rev_raw_bytes, _MM_SHUFFLE(0, 3, 1, 0));
+	// [15,14,13,12,11,10,9,8], [15,14,13,12,11,10,9,8]
+	__m128i zw = _mm_shuffle_epi32(rev_raw_bytes, _MM_SHUFFLE(3, 2, 3, 2));
+
+	// Shift out the extra bits
+	const __m128i shift_offset_s64 = _mm_set1_epi64x(shift_offset);
+	xy = _mm_sll_epi64(xy, shift_offset_s64);
+	zw = _mm_sll_epi64(zw, shift_offset_s64);
+
+	// Combine our result and cast
+	// As u64, we have: {x, y}, but when we cast to u32, we get: {_, x, _, y}
+	return _mm_shuffle_ps(_mm_castsi128_ps(xy), _mm_castsi128_ps(zw), _MM_SHUFFLE(3, 1, 3, 1));
+}
 #endif	// defined(RTM_SSE2_INTRINSICS)
+
+#if defined(RTM_SSE3_INTRINSICS)
+RTM_DISABLE_SECURITY_COOKIE_CHECK RTM_FORCE_NOINLINE
+rtm::vector4f RTM_SIMD_CALL unpack_vector3_f96_sse3_v0(
+	const uint8_t* vector_data,
+	uint32_t bit_offset)
+{
+	// Same principle as NEON v1
+	const uint32_t byte_offset = bit_offset / 8;
+	const uint32_t shift_offset = bit_offset % 8;
+
+	// Load 16 bytes
+	// [0,1,2,3,4,5,6,7], [8,9,10,11,12,13,14,15]
+	const __m128i raw_bytes = _mm_loadu_si128((const __m128i*)(vector_data + byte_offset));
+
+	const __m128i k_byte_swap_mask = _mm_setr_epi32(0x04050607, 0x00010203, 0x0c0d0e0f, 0x08090a0b);
+
+	// [7,6,5,4,3,2,1,0], [15,14,13,12,11,10,9,8]
+	const __m128i rev_raw_bytes = _mm_shuffle_epi8(raw_bytes, k_byte_swap_mask);
+
+	// Select each component and combine our pairs
+	// [7,6,5,4,3,2,1,0], [11,10,9,8,7,6,5,4]
+	__m128i xy = _mm_shuffle_epi32(rev_raw_bytes, _MM_SHUFFLE(0, 3, 1, 0));
+	// [15,14,13,12,11,10,9,8], [15,14,13,12,11,10,9,8]
+	__m128i zw = _mm_shuffle_epi32(rev_raw_bytes, _MM_SHUFFLE(3, 2, 3, 2));
+
+	// Shift out the extra bits
+	const __m128i shift_offset_s64 = _mm_set1_epi64x(shift_offset);
+	xy = _mm_sll_epi64(xy, shift_offset_s64);
+	zw = _mm_sll_epi64(zw, shift_offset_s64);
+
+	// Combine our result and cast
+	// As u64, we have: {x, y}, but when we cast to u32, we get: {_, x, _, y}
+	return _mm_shuffle_ps(_mm_castsi128_ps(xy), _mm_castsi128_ps(zw), _MM_SHUFFLE(3, 1, 3, 1));
+}
+#endif	// defined(RTM_SSE3_INTRINSICS)
 
 static void bm_unpack_vector3_f96_ref(benchmark::State& state)
 {
@@ -352,5 +428,69 @@ static void bm_unpack_vector3_f96_sse2_v0(benchmark::State& state)
 }
 
 BENCHMARK(bm_unpack_vector3_f96_sse2_v0);
+
+static void bm_unpack_vector3_f96_sse2_v1(benchmark::State& state)
+{
+	uint8_t buffer[128] = { 0 };
+	rtm::vector4f v0 = rtm::vector_zero();
+	rtm::vector4f v1 = rtm::vector_zero();
+	rtm::vector4f v2 = rtm::vector_zero();
+	rtm::vector4f v3 = rtm::vector_zero();
+
+	// Prevent compiler from specializing the call with a constant
+	volatile uint32_t bit_offset_0 = 4;
+	volatile uint32_t bit_offset_1 = 5;
+	volatile uint32_t bit_offset_2 = 6;
+	volatile uint32_t bit_offset_3 = 7;
+
+	for (auto _ : state)
+	{
+		v0 = rtm::vector_add(unpack_vector3_f96_sse2_v1(buffer, bit_offset_0), v0);
+		v1 = rtm::vector_add(unpack_vector3_f96_sse2_v1(buffer, bit_offset_1), v1);
+		v2 = rtm::vector_add(unpack_vector3_f96_sse2_v1(buffer, bit_offset_2), v2);
+		v3 = rtm::vector_add(unpack_vector3_f96_sse2_v1(buffer, bit_offset_3), v3);
+	}
+
+	benchmark::DoNotOptimize(buffer);
+	benchmark::DoNotOptimize(v0);
+	benchmark::DoNotOptimize(v1);
+	benchmark::DoNotOptimize(v2);
+	benchmark::DoNotOptimize(v3);
+}
+
+BENCHMARK(bm_unpack_vector3_f96_sse2_v1);
 #endif	// defined(RTM_SSE2_INTRINSICS)
+
+#if defined(RTM_SSE3_INTRINSICS)
+static void bm_unpack_vector3_f96_sse3_v0(benchmark::State& state)
+{
+	uint8_t buffer[128] = { 0 };
+	rtm::vector4f v0 = rtm::vector_zero();
+	rtm::vector4f v1 = rtm::vector_zero();
+	rtm::vector4f v2 = rtm::vector_zero();
+	rtm::vector4f v3 = rtm::vector_zero();
+
+	// Prevent compiler from specializing the call with a constant
+	volatile uint32_t bit_offset_0 = 4;
+	volatile uint32_t bit_offset_1 = 5;
+	volatile uint32_t bit_offset_2 = 6;
+	volatile uint32_t bit_offset_3 = 7;
+
+	for (auto _ : state)
+	{
+		v0 = rtm::vector_add(unpack_vector3_f96_sse3_v0(buffer, bit_offset_0), v0);
+		v1 = rtm::vector_add(unpack_vector3_f96_sse3_v0(buffer, bit_offset_1), v1);
+		v2 = rtm::vector_add(unpack_vector3_f96_sse3_v0(buffer, bit_offset_2), v2);
+		v3 = rtm::vector_add(unpack_vector3_f96_sse3_v0(buffer, bit_offset_3), v3);
+	}
+
+	benchmark::DoNotOptimize(buffer);
+	benchmark::DoNotOptimize(v0);
+	benchmark::DoNotOptimize(v1);
+	benchmark::DoNotOptimize(v2);
+	benchmark::DoNotOptimize(v3);
+}
+
+BENCHMARK(bm_unpack_vector3_f96_sse3_v0);
+#endif	// defined(RTM_SSE3_INTRINSICS)
 #endif // defined(ACL_IMPL_BENCHMARK_UNPACKING)
