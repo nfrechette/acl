3d775f05699f098a57bb3344e75f183c47d7001c
liuchen26183 <liuchen26183@talkweb.com.cn>
2025-05-28 19:17:27 +0800

    acl库ohos适配

---
+++ cmake/Toolchain-OHOS.cmake
@@ -0,0 +1,17 @@
+cmake_minimum_required(VERSION 3.2)
+
+# For OHOS, we just set the platform name as we won't be using CMake to build anything.
+# Instead Gradle is used through CMake.
+
+set(PLATFORM_ANDROID 1)
+set(PLATFORM_NAME "OHOS")
+
+# Remap our CPU instruction set
+if(CPU_INSTRUCTION_SET MATCHES "armv7")
+	set(CPU_INSTRUCTION_SET "armeabi-v7a")
+elseif(CPU_INSTRUCTION_SET MATCHES "arm64")
+	set(CPU_INSTRUCTION_SET "arm64-v8a")
+endif()
+
+# Set our misc asset directory
+set(ACL_OHOS_MISC_DIR ${CMAKE_SOURCE_DIR}/tools/ohos_misc)
