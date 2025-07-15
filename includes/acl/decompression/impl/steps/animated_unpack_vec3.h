c24facae1f981e573d8ea1a6ad2bccdb3d2bba7f
Nicholas Frechette <zeno490@gmail.com>
2025-07-14 22:23:46 -0400

    feat(decompression): add new vec3 unpack variants (disabled)

--- includes/acl/decompression/impl/steps/animated_unpack_vec3.h
+++ includes/acl/decompression/impl/steps/animated_unpack_vec3.h
@@ -35,7 +35,8 @@
 
 // Controls which variant to use for this decompression step
 // 0: ACL 2.1 (baseline)
-// 1: ???
+// 1: Branchless inner loop
+// 2: Constant bit branch only
 #define ACL_IMPL_STEP_CURRENT_GROUP_VARIANT 0
 
 ACL_IMPL_FILE_PRAGMA_PUSH
@@ -171,7 +172,271 @@ namespace acl
 			ACL_IMPL_ANIMATED_PREFETCH(animated_track_data + (animated_track_data_bit_offset / 8) + 63);
 			ACL_IMPL_ANIMATED_PREFETCH(segment_range_data + 48);
 		}
-#elif ACL_IMPL_STEP_CURRENT_GROUP_VARIANT == 1 // 1: ???
+#elif ACL_IMPL_STEP_CURRENT_GROUP_VARIANT == 1 // 1: Branchless inner loop
+		template<class decompression_settings_adapter_type>
+		ACL_IMPL_DEBUG_FORCE_INLINE
+		//RTM_FORCE_NOINLINE
+		RTM_DISABLE_SECURITY_COOKIE_CHECK
+		void unpack_grouped_animated_vector3(
+			const persistent_transform_decompression_context_v0& decomp_context,
+			rtm::vector4f output_scratch[4],
+			uint32_t num_to_unpack,
+			const clip_animated_sampling_context_v0& clip_sampling_context,
+			segment_animated_sampling_context_v0& segment_sampling_context)
+		{
+			const vector_format8 format = get_vector_format<decompression_settings_adapter_type>(decompression_settings_adapter_type::get_vector_format(decomp_context));
+			//const compressed_tracks_version16 version = get_version<decompression_settings_adapter_type>(decomp_context.get_version());
+
+			// See write_format_per_track_data(..) for details
+			//const uint32_t num_raw_bit_rate_bits = version >= compressed_tracks_version16::v02_01_99_1 ? 31U : 32U;
+
+			const uint8_t* format_per_track_data = segment_sampling_context.format_per_track_data;
+			const uint8_t* segment_range_data = segment_sampling_context.segment_range_data;
+			const uint8_t* animated_track_data = segment_sampling_context.animated_track_data;
+			uint32_t animated_track_data_bit_offset = segment_sampling_context.animated_track_data_bit_offset;
+
+			const uint8_t* clip_range_data = clip_sampling_context.clip_range_data;
+
+			for (uint32_t unpack_index = 0; unpack_index < num_to_unpack; ++unpack_index)
+			{
+				rtm::vector4f sample;
+				if (format == vector_format8::vector3f_variable && decompression_settings_adapter_type::is_vector_format_supported(vector_format8::vector3f_variable))
+				{
+					uint32_t num_bits_at_bit_rate = *format_per_track_data;
+					format_per_track_data++;
+
+					// We unpack 3 ways and select the result we want
+					// Unpacking a constant sample is cheap, from CPU L1 with a few instructions
+					// Unpacking a raw sample is nearly the same as a variable sample, we can
+					// do both at the same time
+
+					// Remap 31 to 32
+					// See write_format_per_track_data(..) for details
+					num_bits_at_bit_rate = num_bits_at_bit_rate >= 31 ? 32 : num_bits_at_bit_rate;
+
+					const rtm::vector4f constant_sample = unpack_vector3_u48_unsafe(segment_range_data);
+
+					sample = unpack_vector3_mixed_unsafe(num_bits_at_bit_rate, animated_track_data, animated_track_data_bit_offset);
+
+				#if defined(RTM_NEON64_INTRINSICS) && 0
+					// This should be 2 instructions but Apple Clang generates 6 for it, disable for now
+					const uint32x4_t is_constant_mask = vceqzq_u32(vmovq_n_u32(num_bits_at_bit_rate));
+					sample = RTM_VECTOR4F_SELECT(is_constant_mask, constant_sample, sample);
+				#elif defined(RTM_NEON_INTRINSICS)
+					// Not as efficient but better than vector compare with zero which generates 6 instructions
+					// This yields: sub, asr, dup, bif
+					const uint32x4_t is_constant_mask = vmovq_n_u32(((int32_t)num_bits_at_bit_rate - 1) >> 31);
+					sample = RTM_VECTOR4F_SELECT(is_constant_mask, constant_sample, sample);
+				#else
+					sample = num_bits_at_bit_rate != 0 ? sample : constant_sample;
+				#endif
+
+					animated_track_data_bit_offset += num_bits_at_bit_rate * 3;
+
+					// Remap within our ranges
+					if (decomp_context.has_segments)
+					{
+						// If we aren't raw or constant (32 or 0 bits)
+						if ((num_bits_at_bit_rate & 0x1F) != 0)
+						{
+							// Apply segment range remapping
+							const uint32_t range_entry_size = 3 * sizeof(uint8_t);
+							const uint8_t* segment_range_min_ptr = segment_range_data;
+							const uint8_t* segment_range_extent_ptr = segment_range_min_ptr + range_entry_size;
+
+							const rtm::vector4f segment_range_min = unpack_vector3_u24_unsafe(segment_range_min_ptr);
+							const rtm::vector4f segment_range_extent = unpack_vector3_u24_unsafe(segment_range_extent_ptr);
+
+							sample = rtm::vector_mul_add(sample, segment_range_extent, segment_range_min);
+						}
+
+						segment_range_data += sizeof(uint16_t) * 3;
+					}
+
+					// If we aren't raw (32 bits)
+					if ((num_bits_at_bit_rate & 0x20) == 0)
+					{
+						// Apply clip range remapping
+						const uint32_t range_entry_size = 3 * sizeof(float);
+						const uint32_t sub_track_offset = range_entry_size * 2 * unpack_index;
+						const uint8_t* clip_range_min_ptr = clip_range_data + sub_track_offset;
+						const uint8_t* clip_range_extent_ptr = clip_range_min_ptr + range_entry_size;
+
+						const rtm::vector4f clip_range_min = rtm::vector_load(clip_range_min_ptr);
+						const rtm::vector4f clip_range_extent = rtm::vector_load(clip_range_extent_ptr);
+
+						sample = rtm::vector_mul_add(sample, clip_range_extent, clip_range_min);
+					}
+				}
+				else if (decompression_settings_adapter_type::is_vector_format_supported(vector_format8::vector3f_full))
+				{
+					ACL_ASSERT(format == vector_format8::vector3f_full, "Unexpected vector format");
+					sample = unpack_vector3_96_unsafe(animated_track_data, animated_track_data_bit_offset);
+					animated_track_data_bit_offset += 96;
+				}
+				else
+				{
+					ACL_ASSERT(false, "Should never be reached");
+					sample = rtm::vector_zero();
+				}
+
+				ACL_ASSERT(rtm::vector_is_finite3(sample), "Vector3 is not valid!");
+
+				// TODO: Fill in W component with something sensible?
+
+				// Cache
+				output_scratch[unpack_index] = sample;
+			}
+
+			// Update our pointers
+			segment_sampling_context.format_per_track_data = format_per_track_data;
+			segment_sampling_context.segment_range_data = segment_range_data;
+			segment_sampling_context.animated_track_data_bit_offset = animated_track_data_bit_offset;
+
+			// Prefetch the next cache line even if we don't have any data left
+			// By the time we unpack again, it will have arrived in the CPU cache
+			// If our format is full precision, we have at most 4 samples per cache line
+			// If our format is drop W, we have at most 5.33 samples per cache line
+
+			// If our pointer was already aligned to a cache line before we unpacked our 4 values,
+			// it now points to the first byte of the next cache line. Any offset between 0-63 will fetch it.
+			// If our pointer had some offset into a cache line, we might have spanned 2 cache lines.
+			// If this happens, we probably already read some data from the next cache line in which
+			// case we don't need to prefetch it and we can go to the next one. Any offset after the end
+			// of this cache line will fetch it. For safety, we prefetch 63 bytes ahead.
+			// Prefetch 4 samples ahead in all levels of the CPU cache
+			ACL_IMPL_ANIMATED_PREFETCH(format_per_track_data + 60);
+			ACL_IMPL_ANIMATED_PREFETCH(animated_track_data + (animated_track_data_bit_offset / 8) + 63);
+			ACL_IMPL_ANIMATED_PREFETCH(segment_range_data + 48);
+		}
+#elif ACL_IMPL_STEP_CURRENT_GROUP_VARIANT == 2 // 2: Constant bit branch only
+		template<class decompression_settings_adapter_type>
+		ACL_IMPL_DEBUG_FORCE_INLINE
+		//RTM_FORCE_NOINLINE
+		RTM_DISABLE_SECURITY_COOKIE_CHECK
+		void unpack_grouped_animated_vector3(
+			const persistent_transform_decompression_context_v0& decomp_context,
+			rtm::vector4f output_scratch[4],
+			uint32_t num_to_unpack,
+			const clip_animated_sampling_context_v0& clip_sampling_context,
+			segment_animated_sampling_context_v0& segment_sampling_context)
+		{
+			const vector_format8 format = get_vector_format<decompression_settings_adapter_type>(decompression_settings_adapter_type::get_vector_format(decomp_context));
+			//const compressed_tracks_version16 version = get_version<decompression_settings_adapter_type>(decomp_context.get_version());
+
+			// See write_format_per_track_data(..) for details
+			//const uint32_t num_raw_bit_rate_bits = version >= compressed_tracks_version16::v02_01_99_1 ? 31U : 32U;
+
+			const uint8_t* format_per_track_data = segment_sampling_context.format_per_track_data;
+			const uint8_t* segment_range_data = segment_sampling_context.segment_range_data;
+			const uint8_t* animated_track_data = segment_sampling_context.animated_track_data;
+			uint32_t animated_track_data_bit_offset = segment_sampling_context.animated_track_data_bit_offset;
+
+			const uint8_t* clip_range_data = clip_sampling_context.clip_range_data;
+
+			for (uint32_t unpack_index = 0; unpack_index < num_to_unpack; ++unpack_index)
+			{
+				rtm::vector4f sample;
+				if (format == vector_format8::vector3f_variable && decompression_settings_adapter_type::is_vector_format_supported(vector_format8::vector3f_variable))
+				{
+					uint32_t num_bits_at_bit_rate = *format_per_track_data;
+					format_per_track_data++;
+
+					// We unpack 3 ways and select the result we want
+					// Unpacking a constant sample is cheap, from CPU L1 with a few instructions
+					// Unpacking a raw sample is nearly the same as a variable sample, we can
+					// do both at the same time
+
+					if (num_bits_at_bit_rate == 0) ACL_BRANCH_UNLIKELY
+					{
+						sample = unpack_vector3_u48_unsafe(segment_range_data);
+					}
+					else
+					{
+						// Remap 31 to 32
+						// See write_format_per_track_data(..) for details
+						num_bits_at_bit_rate = num_bits_at_bit_rate >= 31 ? 32 : num_bits_at_bit_rate;
+
+						sample = unpack_vector3_mixed_unsafe(num_bits_at_bit_rate, animated_track_data, animated_track_data_bit_offset);
+						animated_track_data_bit_offset += num_bits_at_bit_rate * 3;
+					}
+
+					// Remap within our ranges
+					if (decomp_context.has_segments)
+					{
+						// If we aren't raw or constant (32 or 0 bits)
+						if ((num_bits_at_bit_rate & 0x1F) != 0)
+						{
+							// Apply segment range remapping
+							const uint32_t range_entry_size = 3 * sizeof(uint8_t);
+							const uint8_t* segment_range_min_ptr = segment_range_data;
+							const uint8_t* segment_range_extent_ptr = segment_range_min_ptr + range_entry_size;
+
+							const rtm::vector4f segment_range_min = unpack_vector3_u24_unsafe(segment_range_min_ptr);
+							const rtm::vector4f segment_range_extent = unpack_vector3_u24_unsafe(segment_range_extent_ptr);
+
+							sample = rtm::vector_mul_add(sample, segment_range_extent, segment_range_min);
+						}
+
+						segment_range_data += sizeof(uint16_t) * 3;
+					}
+
+					// If we aren't raw (32 bits)
+					if ((num_bits_at_bit_rate & 0x20) == 0)
+					{
+						// Apply clip range remapping
+						const uint32_t range_entry_size = 3 * sizeof(float);
+						const uint32_t sub_track_offset = range_entry_size * 2 * unpack_index;
+						const uint8_t* clip_range_min_ptr = clip_range_data + sub_track_offset;
+						const uint8_t* clip_range_extent_ptr = clip_range_min_ptr + range_entry_size;
+
+						const rtm::vector4f clip_range_min = rtm::vector_load(clip_range_min_ptr);
+						const rtm::vector4f clip_range_extent = rtm::vector_load(clip_range_extent_ptr);
+
+						sample = rtm::vector_mul_add(sample, clip_range_extent, clip_range_min);
+					}
+				}
+				else if (decompression_settings_adapter_type::is_vector_format_supported(vector_format8::vector3f_full))
+				{
+					ACL_ASSERT(format == vector_format8::vector3f_full, "Unexpected vector format");
+					sample = unpack_vector3_96_unsafe(animated_track_data, animated_track_data_bit_offset);
+					animated_track_data_bit_offset += 96;
+				}
+				else
+				{
+					ACL_ASSERT(false, "Should never be reached");
+					sample = rtm::vector_zero();
+				}
+
+				ACL_ASSERT(rtm::vector_is_finite3(sample), "Vector3 is not valid!");
+
+				// TODO: Fill in W component with something sensible?
+
+				// Cache
+				output_scratch[unpack_index] = sample;
+			}
+
+			// Update our pointers
+			segment_sampling_context.format_per_track_data = format_per_track_data;
+			segment_sampling_context.segment_range_data = segment_range_data;
+			segment_sampling_context.animated_track_data_bit_offset = animated_track_data_bit_offset;
+
+			// Prefetch the next cache line even if we don't have any data left
+			// By the time we unpack again, it will have arrived in the CPU cache
+			// If our format is full precision, we have at most 4 samples per cache line
+			// If our format is drop W, we have at most 5.33 samples per cache line
+
+			// If our pointer was already aligned to a cache line before we unpacked our 4 values,
+			// it now points to the first byte of the next cache line. Any offset between 0-63 will fetch it.
+			// If our pointer had some offset into a cache line, we might have spanned 2 cache lines.
+			// If this happens, we probably already read some data from the next cache line in which
+			// case we don't need to prefetch it and we can go to the next one. Any offset after the end
+			// of this cache line will fetch it. For safety, we prefetch 63 bytes ahead.
+			// Prefetch 4 samples ahead in all levels of the CPU cache
+			ACL_IMPL_ANIMATED_PREFETCH(format_per_track_data + 60);
+			ACL_IMPL_ANIMATED_PREFETCH(animated_track_data + (animated_track_data_bit_offset / 8) + 63);
+			ACL_IMPL_ANIMATED_PREFETCH(segment_range_data + 48);
+		}
 #endif
 
 		template<class decompression_settings_adapter_type>
