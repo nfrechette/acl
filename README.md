8a899b73c6706b385b7aab4bb335c160a35be099
Nicholas Frechette <zeno490@gmail.com>
2025-09-12 21:46:47 -0400

    feat: add support for ARM64EC with MSVC

--- README.md
+++ README.md
@@ -45,7 +45,7 @@ Much thought was put into designing the library for it to be as flexible and pow
 
 Continuous integration tests a variety of platforms and configurations but it generally runs as-is anywhere where C++11 (or later) is supported. CI currently tests:
 
-*  Windows VS2022: x86, x64, ARM64
+*  Windows VS2022: x86, x64, ARM64, ARM64EC
 *  Linux GCC 12+: x86, x64
 *  Linux Clang 15+: x86, x64
 *  OS X XCode 15+: ARM64
--- make.py
+++ make.py
@@ -45,7 +45,7 @@ def parse_argv():
 	target = parser.add_argument_group(title='Target')
 	target.add_argument('-compiler', choices=['vs2015', 'vs2017', 'vs2019', 'vs2022', 'vs2019-clang', 'vs2022-clang', 'android', 'clang4', 'clang5', 'clang6', 'clang7', 'clang8', 'clang9', 'clang10', 'clang11', 'clang12', 'clang13', 'clang14', 'clang15', 'clang16', 'clang17', 'clang18', 'gcc4.9', 'gcc5', 'gcc6', 'gcc7', 'gcc8', 'gcc9', 'gcc10', 'gcc11', 'gcc12', 'gcc13', 'osx', 'ios', 'emscripten'], help='Defaults to the host system\'s default compiler')
 	target.add_argument('-config', choices=['Debug', 'Release'], type=str.capitalize)
-	target.add_argument('-cpu', choices=['x86', 'x64', 'armv7', 'arm64', 'wasm'], help='Defaults to the host system\'s architecture')
+	target.add_argument('-cpu', choices=['x86', 'x64', 'armv7', 'arm64', 'arm64ec', 'wasm'], help='Defaults to the host system\'s architecture')
 	target.add_argument('-cpp_version', choices=['11', '14', '17', '20'], help='Defaults to C++11')
 
 	misc = parser.add_argument_group(title='Miscellaneous')
@@ -155,7 +155,11 @@ def parse_argv():
 			is_arm_supported = True
 
 		if not is_arm_supported:
-			print('arm64 is only supported with VS2017, VS2019, OS X (M1 processors), Linux, Android, and iOS')
+			print('arm64 is only supported with VS2017, VS2019, VS2022, OS X (M* processors), Linux, Android, and iOS')
+			sys.exit(1)
+	elif args.cpu == 'arm64ec':
+		if not args.compiler in ['vs2019', 'vs2022']:
+			print('arm64ec is only supported with VS2019 and VS2022')
 			sys.exit(1)
 	elif args.cpu == 'armv7':
 		if not args.compiler == 'android':
